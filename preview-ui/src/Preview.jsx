25784461058807110711cfc88c78e47aca309123
yz-yu <aryu0112@gmail.com>
2023-12-21 17:15:35 +0800

    [vx.dev] prompt: {   "prompt": "\r\n\r\n一个写实风格的计算器 UI\nadd 3d style to buttons",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/13651389/827e4c28-66c1-4ba1-b36d-c261a3e44871"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -3,38 +3,23 @@ import React from 'react';
 const CalculatorUI = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="bg-white p-4 shadow-lg rounded-lg">
-        <div className="bg-gray-200 p-2 rounded text-right">
-          <div className="text-gray-600 text-sm">Iddi</div>
-          <div className="text-gray-800 text-2xl">192.6</div>
+      <div className="bg-white p-6 rounded-lg shadow-lg" style={{ width: '300px' }}>
+        <div className="bg-gray-200 p-4 rounded mb-4">
+          <div className="text-right text-gray-700 text-lg" aria-label="Calculator Display">
+            0
+          </div>
         </div>
-        <div className="grid grid-cols-4 gap-2 mt-4">
-          {['7', '8', '9', '×'].map((text) => (
-            <button key={text} className="bg-gray-300 p-4 rounded text-xl focus:outline-none">
-              {text}
+        <div className="grid grid-cols-4 gap-2">
+          {['7', '8', '9', '×', '4', '5', '6', '-', '1', '2', '3', '+', '0', '%', 'C', '='].map((buttonLabel) => (
+            <button
+              key={buttonLabel}
+              className="bg-gray-300 text-gray-700 font-semibold py-3 rounded shadow-inner focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50"
+              style={{ boxShadow: 'inset 0 4px rgba(0,0,0,0.2)' }}
+              aria-label={`Button ${buttonLabel}`}
+            >
+              {buttonLabel}
             </button>
           ))}
-          <button className="bg-red-500 p-4 rounded text-xl text-white focus:outline-none">
-            C
-          </button>
-          {['4', '5', '6', '-'].map((text) => (
-            <button key={text} className="bg-gray-300 p-4 rounded text-xl focus:outline-none">
-              {text}
-            </button>
-          ))}
-          <button className="bg-gray-300 p-4 rounded text-xl focus:outline-none">M</button>
-          {['1', '2', '3', '+'].map((text) => (
-            <button key={text} className="bg-gray-300 p-4 rounded text-xl focus:outline-none">
-              {text}
-            </button>
-          ))}
-          <button className="bg-gray-300 p-4 rounded text-xl focus:outline-none">√</button>
-          <button className="bg-gray-300 p-4 rounded text-xl focus:outline-none">0</button>
-          <button className="bg-gray-300 p-4 rounded text-xl focus:outline-none">%</button>
-          <button className="bg-gray-300 p-4 rounded text-xl focus:outline-none">+</button>
-          <button className="col-span-2 bg-blue-500 p-4 rounded text-xl text-white focus:outline-none">
-            =
-          </button>
         </div>
       </div>
     </div>
