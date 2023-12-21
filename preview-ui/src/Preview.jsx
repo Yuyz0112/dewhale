16e635b851340c97488b02101fa55aa1e6f2fc1d
yz-yu <aryu0112@gmail.com>
2023-12-21 17:15:56 +0800

    [vx.dev] prompt: {   "prompt": "\r\n\r\nRetirement goals calculator\nbuild error from your previous code\r\n\r\n```\r\n\"Tshirt\" is not exported by \"node_modules/lucide-react/dist/esm/lucide-react.js\"\r\n```",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/13651389/cda6f7bc-95c4-48a0-ae6a-e52e6b38f4ad"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,71 +1,78 @@
-import { useState } from 'react';
-import { Button } from "@/components/ui/button";
-import { Slider } from "@/components/ui/slider";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Input } from "@/components/ui/input";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Plane, Home, Car, Heart, Tshirt, Hammer, CreditCard, Briefcase } from 'lucide-react';
+import React from 'react';
+import { Heart, Home, Car, Medical, Edit, Trash, Plus, Minus } from 'lucide-react';
+import { Button, Slider, Input, Label } from "@/components/ui";
 
-export default function RetirementGoalsCalculator() {
-  const [selectedGoal, setSelectedGoal] = useState('Travel');
-  const [cost, setCost] = useState(20000);
-  const [frequency, setFrequency] = useState('Every 2 years');
-  const [ageRange, setAgeRange] = useState([65, 89]);
-
+const RetirementGoalsCalculator = () => {
   return (
-    <div className="bg-gray-800 text-white min-h-screen flex items-center justify-center p-4">
-      <div className="max-w-4xl w-full">
-        <header className="flex justify-between items-center mb-6">
-          <h1 className="text-xl font-bold">HSBC</h1>
+    <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center p-4">
+      <header className="text-red-600 text-3xl font-bold mb-8">HSBC</header>
+      <div className="w-full max-w-4xl bg-gray-800 rounded-lg p-8">
+        <h2 className="text-2xl font-semibold mb-4">Add your dream retirement goals</h2>
+        <p className="text-gray-400 mb-8">What dream retirement goals would you like to include?</p>
+        <div className="grid grid-cols-4 gap-4 mb-8">
+          <div className="flex flex-col items-center justify-center p-4 bg-gray-700 rounded">
+            <Heart className="w-8 h-8 mb-2" />
+            <span>Travel</span>
+          </div>
+          <div className="flex flex-col items-center justify-center p-4 bg-gray-700 rounded">
+            <Home className="w-8 h-8 mb-2" />
+            <span>Rental</span>
+          </div>
+          <div className="flex flex-col items-center justify-center p-4 bg-gray-700 rounded">
+            <Car className="w-8 h-8 mb-2" />
+            <span>Car</span>
+          </div>
+          <div className="flex flex-col items-center justify-center p-4 bg-gray-700 rounded">
+            <Medical className="w-8 h-8 mb-2" />
+            <span>Medical</span>
+          </div>
+          {/* Additional goal icons can be added here */}
+        </div>
+        <div className="bg-gray-700 p-4 rounded-lg flex items-center justify-between mb-4">
           <div className="flex items-center">
-            <span className="text-sm mr-2">Step 3 of 4</span>
-            <div className="w-32 h-1 bg-gray-700 rounded-full overflow-hidden">
-              <div className="w-3/4 h-full bg-red-600"></div>
-            </div>
+            <Heart className="w-6 h-6 mr-2" />
+            <span className="font-semibold">Travel</span>
           </div>
-        </header>
-        <main className="bg-gray-700 rounded-lg p-6">
-          <h2 className="text-2xl font-bold mb-4">Add your dream retirement goals</h2>
-          <p className="text-gray-300 mb-8">What dream retirement goals would you like to include?</p>
-          <Tabs defaultValue={selectedGoal} onValueChange={setSelectedGoal} className="mb-8">
-            <TabsList>
-              <TabsTrigger value="Travel"><Plane className="w-5 h-5" /></TabsTrigger>
-              <TabsTrigger value="Rental"><Home className="w-5 h-5" /></TabsTrigger>
-              <TabsTrigger value="Car"><Car className="w-5 h-5" /></TabsTrigger>
-              <TabsTrigger value="Medical"><Heart className="w-5 h-5" /></TabsTrigger>
-              <TabsTrigger value="Legacy"><Briefcase className="w-5 h-5" /></TabsTrigger>
-              <TabsTrigger value="Clothes"><Tshirt className="w-5 h-5" /></TabsTrigger>
-              <TabsTrigger value="Construction"><Hammer className="w-5 h-5" /></TabsTrigger>
-              <TabsTrigger value="Payment"><CreditCard className="w-5 h-5" /></TabsTrigger>
-            </TabsList>
-          </Tabs>
-          <div className="bg-gray-800 p-4 rounded-lg">
-            <h3 className="text-lg font-semibold mb-2">Your dream retirement goals</h3>
-            <p className="text-gray-400 mb-4">Set your dream retirement goals in today's dollars</p>
-            <div className="mb-6">
-              <label htmlFor="cost" className="block text-sm font-medium text-gray-300 mb-1">Cost</label>
-              <Input id="cost" type="number" value={cost} onChange={(e) => setCost(e.target.value)} className="w-full mb-2" />
-              <label htmlFor="frequency" className="block text-sm font-medium text-gray-300 mb-1">Frequency</label>
-              <Select id="frequency" defaultValue={frequency} onValueChange={setFrequency} className="w-full mb-2">
-                <SelectTrigger className="w-full">
-                  <SelectValue />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="Every year">Every year</SelectItem>
-                  <SelectItem value="Every 2 years">Every 2 years</SelectItem>
-                  <SelectItem value="Every 5 years">Every 5 years</SelectItem>
-                </SelectContent>
-              </Select>
-              <label htmlFor="age-range" className="block text-sm font-medium text-gray-300 mb-1">Age period</label>
-              <Slider id="age-range" defaultValue={ageRange} onValueChange={setAgeRange} min={50} max={100} step={1} className="w-full" />
-            </div>
-            <div className="flex justify-between items-center">
-              <Button variant="outline" className="text-gray-400 border-gray-600">Previous</Button>
-              <Button className="bg-red-600">Next</Button>
-            </div>
-          </div>
-        </main>
+          <Button variant="ghost" className="text-red-600">
+            <Trash className="w-5 h-5" />
+          </Button>
+        </div>
+        <div className="flex items-center justify-between mb-4">
+          <Label className="flex-1 mr-4">
+            Cost
+            <Input className="mt-1" placeholder="20,000" />
+          </Label>
+          <Label className="flex-1">
+            Frequency
+            <Input className="mt-1" placeholder="Every 2 years" />
+          </Label>
+        </div>
+        <div className="flex items-center mb-8">
+          <Label className="flex-1 mr-4">
+            Age period
+            <Slider className="mt-1" defaultValue={[65, 89]} />
+          </Label>
+        </div>
+        <div className="flex justify-between">
+          <Button variant="outline" className="text-gray-400 border-gray-600">
+            Previous
+          </Button>
+          <Button className="bg-red-600">
+            Next
+          </Button>
+        </div>
+      </div>
+      <div className="flex justify-between items-center w-full max-w-4xl mt-4">
+        <div className="flex items-center">
+          <div className="w-2 h-2 bg-gray-400 rounded-full mr-2"></div>
+          <div className="w-2 h-2 bg-gray-400 rounded-full mr-2"></div>
+          <div className="w-2 h-2 bg-red-600 rounded-full mr-2"></div>
+          <div className="w-2 h-2 bg-gray-400 rounded-full"></div>
+        </div>
+        <span className="text-gray-400">Step 03/04</span>
       </div>
     </div>
   );
-}
+};
+
+export default RetirementGoalsCalculator;
