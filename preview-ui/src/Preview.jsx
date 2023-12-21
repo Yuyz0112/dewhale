91e8fa6622c613b92b396d314bd801ff3a9255e6
yz-yu <aryu0112@gmail.com>
2023-12-21 22:44:35 +0800

    [vx.dev] prompt: {   "prompt": "Build a Web-based file manager \r\n\r\n Clean, modern user interface using UI with light and dark modes.\r\n Optimistic, instant UI updates + loading states with  query.\r\n Drag n‘ drop local + remote uploads.\r\nSupport for individual files and folders\r\nSupports multiple file formats and displays icons according to different types.\r\nSupport for compressing, decompressing, moving and copying files\r\n\r\n\r\n\r\n\n存在运行时错误，重新调整保证正确性\nTableHeader 的类名不正确，请调整为与 TableCell 可以对应。\ntry again..",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/18740395/85f7e24e-3ebe-4e2b-bde8-e4f1c222f7af",     "https://github.com/Yuyz0112/vx.dev/assets/18740395/d16c3e56-d247-403f-ae6b-1d5c2c01332a"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,115 +1,112 @@
 import React from 'react';
-import { Folder, FileText, UploadCloud, Plus, FilePlus, Compress, Copy, Move, Trash2 } from 'lucide-react';
-import { Table, TableBody, TableCaption, TableCell, TableHeader, TableRow } from "@/components/ui/table";
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Toaster, useToast } from "@/components/ui/toaster";
-import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from "@/components/ui/context-menu";
-import { TooltipProvider, Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
-import { Switch } from "@/components/ui/switch";
+import { Folder, File, Upload, Plus, Minus, Download, Edit, Trash, Image, Video, Music, FileText, Archive, ChevronDown, ChevronUp, Sun, Moon, LogOut } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Table, TableBody, TableCell, TableHeader, TableHead, TableRow } from '@/components/ui/table';
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
+import { TooltipProvider, Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 
 export default function FileManager() {
-  const { toast } = useToast();
-
-  // Dummy data for files and folders
-  const files = [
-    { name: 'Directory', size: '64 B', modified: 'Nov 22, 2023 06:26 AM', created: 'Nov 22, 2023 06:26 AM', type: 'folder' },
-    { name: 'Facebook Lite_381.0.0.3.100_apkcombo.com.apk', size: '2.22 MiB', modified: 'Nov 11, 2023 04:38 PM', created: 'Nov 22, 2023 06:22 AM', type: 'file' },
-    { name: 'WhatsApp Business_2.23.24.18_apkcombo.com.apk', size: '0 B', modified: 'Nov 11, 2023 04:26 PM', created: 'Nov 22, 2023 06:22 AM', type: 'file' },
-  ];
-
-  // Function to simulate file upload and show toast
-  const handleUpload = () => {
-    toast({
-      title: 'Files uploaded.',
-      description: 'Your files have been successfully uploaded.',
-    });
-  };
-
   return (
     <TooltipProvider>
-      <div className="flex flex-col h-screen">
-        <header className="bg-white p-4 shadow-md flex justify-between items-center">
-          <h1 className="text-xl font-semibold">Turbo Folder</h1>
-          <div>
-            <Button onClick={handleUpload} className="mr-2">
-              <UploadCloud className="w-4 h-4 mr-1" /> Upload
-            </Button>
-            <Button className="mr-2">
-              <Plus className="w-4 h-4 mr-1" /> New folder
-            </Button>
-            <Button>
-              <FilePlus className="w-4 h-4 mr-1" /> New file
-            </Button>
-          </div>
-        </header>
-        <div className="flex flex-1 overflow-hidden">
-          <div className="bg-white w-64 p-6 hidden sm:block">
-            <div className="flex items-center mb-4">
+      <div className="flex flex-col h-screen bg-gray-100 dark:bg-gray-800">
+        {/* Sidebar */}
+        <div className="bg-white dark:bg-gray-900 w-64 p-6 hidden sm:block">
+          <div className="text-gray-900 dark:text-white font-bold mb-4">Turbo Folder</div>
+          <div className="space-y-2">
+            <Button variant="ghost" className="justify-start w-full">
               <Folder className="w-5 h-5 mr-2" /> My Drive
-            </div>
-            <div className="flex items-center mb-4">
-              <Trash2 className="w-5 h-5 mr-2" /> Trash
-            </div>
-            <div className="flex items-center mb-4">
-              <FileText className="w-5 h-5 mr-2" /> Users
-            </div>
-            <div className="flex items-center">
+            </Button>
+            <Button variant="ghost" className="justify-start w-full">
+              <Trash className="w-5 h-5 mr-2" /> Trash
+            </Button>
+            <Button variant="ghost" className="justify-start w-full">
+              <User className="w-5 h-5 mr-2" /> Users
+            </Button>
+            <Button variant="ghost" className="justify-start w-full">
               <FileText className="w-5 h-5 mr-2" /> Logs
+            </Button>
+          </div>
+        </div>
+
+        {/* Main content */}
+        <div className="flex-1 p-6">
+          <div className="flex justify-between items-center mb-4">
+            <div className="flex space-x-2">
+              <Button variant="ghost">
+                <ChevronUp className="w-5 h-5" />
+              </Button>
+              <Button variant="ghost">
+                <ChevronDown className="w-5 h-5" />
+              </Button>
+            </div>
+            <div className="flex space-x-2">
+              <Button variant="solid" className="flex items-center">
+                <Upload className="w-5 h-5 mr-2" /> Upload
+              </Button>
+              <Button variant="solid" className="flex items-center">
+                <Plus className="w-5 h-5 mr-2" /> New folder
+              </Button>
+              <Button variant="solid" className="flex items-center">
+                <File className="w-5 h-5 mr-2" /> New file
+              </Button>
             </div>
           </div>
-          <main className="flex-1 overflow-y-auto p-4">
-            <Table>
-              <TableCaption>Total {files.length} items in the current directory.</TableCaption>
-              <TableHeader>
-                <TableRow>
-                  <TableCell><Checkbox /></TableCell>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Size</TableCell>
-                  <TableCell>Last modified</TableCell>
-                  <TableCell>Created at</TableCell>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {files.map((file, index) => (
-                  <TableRow key={index}>
-                    <TableCell><Checkbox /></TableCell>
-                    <TableCell>
-                      {file.type === 'folder' ? <Folder className="inline w-4 h-4 mr-2" /> : <FileText className="inline w-4 h-4 mr-2" />}
-                      {file.name}
-                    </TableCell>
-                    <TableCell>{file.size}</TableCell>
-                    <TableCell>{file.modified}</TableCell>
-                    <TableCell>{file.created}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </main>
+
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[50px]"></TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Size</TableHead>
+                <TableHead>Last modified</TableHead>
+                <TableHead>Created at</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {/* Example file */}
+              <TableRow>
+                <TableCell>
+                  <Checkbox />
+                </TableCell>
+                <TableCell>
+                  <Folder className="w-5 h-5 mr-2 inline" />
+                  Directory
+                </TableCell>
+                <TableCell>64 B</TableCell>
+                <TableCell>Nov 22, 2023</TableCell>
+                <TableCell>Nov 22, 2023</TableCell>
+              </TableRow>
+              {/* Example file */}
+              <TableRow>
+                <TableCell>
+                  <Checkbox />
+                </TableCell>
+                <TableCell>
+                  <File className="w-5 h-5 mr-2 inline" />
+                  Document.pdf
+                </TableCell>
+                <TableCell>1.2 MB</TableCell>
+                <TableCell>Nov 20, 2023</TableCell>
+                <TableCell>Nov 18, 2023</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
         </div>
-        <footer className="bg-white p-4 shadow-md flex justify-between items-center">
-          <div>
-            <Switch /> Theme
-          </div>
-          <Button variant="outline">Sign out</Button>
-        </footer>
-        <Toaster />
-        <ContextMenu>
-          <ContextMenuTrigger asChild>
-            <Button className="absolute bottom-4 right-4">...</Button>
-          </ContextMenuTrigger>
-          <ContextMenuContent>
-            <ContextMenuItem>Open in new tab</ContextMenuItem>
-            <ContextMenuItem>Download</ContextMenuItem>
-            <ContextMenuItem>Share</ContextMenuItem>
-            <ContextMenuItem>Rename</ContextMenuItem>
-            <ContextMenuItem>Move</ContextMenuItem>
-            <ContextMenuItem>Copy</ContextMenuItem>
-            <ContextMenuItem>Compress</ContextMenuItem>
-            <ContextMenuItem>Extract</ContextMenuItem>
-            <ContextMenuItem>Move to trash</ContextMenuItem>
-          </ContextMenuContent>
-        </ContextMenu>
+
+        {/* Footer */}
+        <div className="bg-white dark:bg-gray-900 p-4 shadow-md flex justify-between items-center">
+          <ToggleGroup type="single" defaultValue="light">
+            <ToggleGroupItem value="light" aria-label="Light mode">
+              <Sun className="w-5 h-5" />
+            </ToggleGroupItem>
+            <ToggleGroupItem value="dark" aria-label="Dark mode">
+              <Moon className="w-5 h-5" />
+            </ToggleGroupItem>
+          </ToggleGroup>
+          <Button variant="ghost" className="flex items-center">
+            <LogOut className="w-5 h-5 mr-2" /> Sign out
+          </Button>
+        </div>
       </div>
     </TooltipProvider>
   );
