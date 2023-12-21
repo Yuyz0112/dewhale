25eeaf7243f5b81e244f6c1433978e3edc9dd9f5
yz-yu <aryu0112@gmail.com>
2023-12-21 22:06:08 +0800

    [vx.dev] prompt: {   "prompt": "Build a Web-based file manager \r\n\r\n Clean, modern user interface using UI with light and dark modes.\r\n Optimistic, instant UI updates + loading states with  query.\r\n Drag n‘ drop local + remote uploads.\r\nSupport for individual files and folders\r\nSupports multiple file formats and displays icons according to different types.\r\nSupport for compressing, decompressing, moving and copying files\r\n\r\n\r\n\r\n\n存在运行时错误，重新调整保证正确性",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/18740395/85f7e24e-3ebe-4e2b-bde8-e4f1c222f7af",     "https://github.com/Yuyz0112/vx.dev/assets/18740395/d16c3e56-d247-403f-ae6b-1d5c2c01332a"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,114 +1,117 @@
 import React from 'react';
-import { Folder, File, FileText, Image, Music, Video, Archive, Upload, Plus, Download, Trash, Edit, Copy, ArrowRight, Home } from 'lucide-react';
-import { Button } from "@/components/ui/button";
-import { Table, TableBody, TableCell, TableHead, TableRow } from "@/components/ui/table";
-import { Toaster, useToast } from "@/components/ui/toast";
-import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from "@/components/ui/context-menu";
-import { Switch } from "@/components/ui/switch";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { Folder, File, FileText, Image, Music, Video, Archive, Upload, Plus, Edit, Trash, Download, ArrowRight, Settings, User } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Switch } from '@/components/ui/switch';
 
 export default function FileManager() {
-  const { toast } = useToast();
-
-  // Simulate file upload toast
-  const handleUpload = () => {
-    toast({
-      title: "Files uploading...",
-      description: "Your files are being uploaded.",
-    });
-  };
-
-  // File icon selector based on file type
-  const fileIcon = (type) => {
-    switch (type) {
-      case 'folder':
-        return <Folder />;
-      case 'image':
-        return <Image />;
-      case 'music':
-        return <Music />;
-      case 'video':
-        return <Video />;
-      case 'archive':
-        return <Archive />;
-      case 'text':
-      default:
-        return <FileText />;
-    }
-  };
-
   return (
     <div className="flex h-screen bg-gray-100 dark:bg-gray-800">
-      <Toaster />
-      <div className="bg-white dark:bg-gray-700 w-64 p-6 hidden sm:block">
-        <div className="text-lg font-bold mb-4">Turbo Folder</div>
-        <div className="space-y-2">
-          <Button variant="ghost" className="justify-start"><Home className="mr-2" />My Drive</Button>
-          <Button variant="ghost" className="justify-start"><Trash className="mr-2" />Trash</Button>
-          <Button variant="ghost" className="justify-start"><User className="mr-2" />Users</Button>
-          <Button variant="ghost" className="justify-start"><File className="mr-2" />Logs</Button>
+      <div className="bg-white dark:bg-gray-900 w-64 p-6 hidden sm:block">
+        <div className="flex items-center justify-between mb-6">
+          <h1 className="text-xl font-bold dark:text-white">Turbo Folder</h1>
+          <Switch />
+        </div>
+        <nav>
+          <ul>
+            <li className="flex items-center space-x-2 mb-4">
+              <Folder className="text-gray-500 dark:text-gray-400" />
+              <span className="dark:text-gray-300">My Drive</span>
+            </li>
+            <li className="flex items-center space-x-2 mb-4">
+              <Trash className="text-gray-500 dark:text-gray-400" />
+              <span className="dark:text-gray-300">Trash</span>
+            </li>
+            <li className="flex items-center space-x-2 mb-4">
+              <User className="text-gray-500 dark:text-gray-400" />
+              <span className="dark:text-gray-300">Users</span>
+            </li>
+            <li className="flex items-center space-x-2">
+              <Settings className="text-gray-500 dark:text-gray-400" />
+              <span className="dark:text-gray-300">Logs</span>
+            </li>
+          </ul>
+        </nav>
+        <div className="absolute bottom-0 left-0 p-6 w-64">
+          <Button variant="outline" className="dark:border-gray-700 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white">
+            Sign out
+          </Button>
         </div>
       </div>
       <div className="flex-1 p-6">
         <div className="flex justify-between items-center mb-4">
-          <div className="flex items-center">
-            <Button variant="ghost" className="mr-2"><ArrowRight /></Button>
-            <Button variant="ghost" className="mr-2"><ArrowRight /></Button>
-            <div className="text-lg font-bold">My Drive</div>
+          <div className="flex space-x-2">
+            <Button variant="outline" className="dark:border-gray-700 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white">
+              <Upload className="w-4 h-4 mr-2" /> Upload
+            </Button>
+            <Button variant="outline" className="dark:border-gray-700 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white">
+              <Plus className="w-4 h-4 mr-2" /> New folder
+            </Button>
+            <Button variant="outline" className="dark:border-gray-700 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white">
+              <FileText className="w-4 h-4 mr-2" /> New file
+            </Button>
           </div>
-          <div className="flex items-center">
-            <Button onClick={handleUpload} className="mr-2"><Upload className="mr-2" />Upload</Button>
-            <Button className="mr-2"><Plus className="mr-2" />New folder</Button>
-            <Button className="mr-2"><File className="mr-2" />New file</Button>
-          </div>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="outline" className="dark:border-gray-700 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white">
+                  <ArrowRight className="w-4 h-4" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>Next</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableHead>Name</TableHead>
-              <TableHead>Size</TableHead>
-              <TableHead>Last modified</TableHead>
-              <TableHead>Created at</TableHead>
-              <TableHead>Actions</TableHead>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {/* Example file row */}
-            <TableRow>
-              <TableCell>{fileIcon('folder')} Directory</TableCell>
-              <TableCell>64 B</TableCell>
-              <TableCell>Nov 22, 2023</TableCell>
-              <TableCell>Nov 22, 2023</TableCell>
-              <TableCell>
-                <ContextMenu>
-                  <ContextMenuTrigger asChild>
-                    <Button variant="ghost">...</Button>
-                  </ContextMenuTrigger>
-                  <ContextMenuContent>
-                    <ContextMenuItem>Open in new tab</ContextMenuItem>
-                    <ContextMenuItem>Download</ContextMenuItem>
-                    <ContextMenuItem>Share</ContextMenuItem>
-                    <ContextMenuItem>Rename</ContextMenuItem>
-                    <ContextMenuItem>Move</ContextMenuItem>
-                    <ContextMenuItem>Copy</ContextMenuItem>
-                    <ContextMenuItem>Compress</ContextMenuItem>
-                    <ContextMenuItem>Extract</ContextMenuItem>
-                    <ContextMenuItem>Move to trash</ContextMenuItem>
-                  </ContextMenuContent>
-                </ContextMenu>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-        <div className="absolute bottom-0 left-0 right-0 p-4 bg-white dark:bg-gray-700 flex justify-between items-center">
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Button variant="ghost"><Home /></Button>
-            </TooltipTrigger>
-            <TooltipContent>Change Theme</TooltipContent>
-          </Tooltip>
-          <Switch />
-          <Button variant="ghost">Sign out</Button>
+        <div className="bg-white dark:bg-gray-900 rounded shadow p-6">
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableHeader className="w-[100px] dark:text-gray-300">Name</TableHeader>
+                <TableHeader className="dark:text-gray-300">Size</TableHeader>
+                <TableHeader className="dark:text-gray-300">Last modified</TableHeader>
+                <TableHeader className="dark:text-gray-300">Created at</TableHeader>
+                <TableHeader className="dark:text-gray-300">Actions</TableHeader>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {/* Placeholder for file and folder rows */}
+              <TableRow>
+                <TableCell className="font-medium dark:text-white">
+                  <div className="flex items-center space-x-2">
+                    <Folder className="text-yellow-500" />
+                    <span>Directory</span>
+                  </div>
+                </TableCell>
+                <TableCell className="dark:text-gray-300">64 B</TableCell>
+                <TableCell className="dark:text-gray-300">Nov 22, 2023</TableCell>
+                <TableCell className="dark:text-gray-300">Nov 22, 2023</TableCell>
+                <TableCell>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="outline" className="dark:border-gray-700 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white">
+                        <Edit className="w-4 h-4" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      <DropdownMenuItem>Open in new tab</DropdownMenuItem>
+                      <DropdownMenuItem>Download</DropdownMenuItem>
+                      <DropdownMenuItem>Share</DropdownMenuItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>Rename</DropdownMenuItem>
+                      <DropdownMenuItem>Move</DropdownMenuItem>
+                      <DropdownMenuItem>Copy</DropdownMenuItem>
+                      <DropdownMenuItem>Compress</DropdownMenuItem>
+                      <DropdownMenuItem>Extract</DropdownMenuItem>
+                      <DropdownMenuItem>Move to trash</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </TableCell>
+              </TableRow>
+              {/* Repeat for each file/folder */}
+            </TableBody>
+          </Table>
         </div>
       </div>
     </div>
