17dbb7ecfe5effe5fa836531adbf3ce5f88b43e4
yz-yu <aryu0112@gmail.com>
2023-12-21 16:49:17 +0800

    [vx.dev] prompt: {   "prompt": "Create an email client hero component with centered text and images. Remove the image and make it dark mode. Move the image under the text. Add a subtle background pattern and black & white gradient. Use a darker border and gradient on the heading. Make it full height. Clean up text. Regenerate the copy with a catchy headline.\r\n\r\nAdd a 3 column grid features section in the same style. Remove images from features. Improve features header and descriptions. Improve copy for 3 features for an email app. Add 3 more features. Add subtle borders between features and increase spacing. Refine borders to be more subtle.\r\n\r\nAdd space between header and features. Add icons matching feature titles. Add rounded borders and center align icons. Add padding to icon containers and reduce icon opacity to blend with background.\nsome other version\nUse a more contrasting color for headlines",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,104 +1,64 @@
-import { Mail, Settings, Star, InboxIcon, Wifi, Clock, Edit, Camera, File } from 'lucide-react';
-import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Mail, Star, Wifi, Clock, Settings, InboxIcon, Edit, Camera } from 'lucide-react';
+import { Card, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 
-const HeroSection = () => {
+export default function EmailClientComponent() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-[url('/images/pattern.png')] bg-cover bg-fixed text-white">
-      <div className="text-center p-4">
+    <div className="flex flex-col h-screen bg-gradient-to-br from-gray-900 to-black text-white">
+      <div className="flex-1 flex flex-col items-center justify-center bg-[url('https://www.transparenttextures.com/patterns/asfalt-dark.png')]">
         <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-gray-700 to-gray-900 mb-4">
           Revolutionize Your Inbox
         </h1>
-        <p className="max-w-md mx-auto mb-6">
-          Experience the ultimate email client that brings efficiency and style to your email management. Stay connected, organized, and in control.
+        <p className="max-w-md text-center mb-8">
+          Experience the future of email with our cutting-edge client. Organize, prioritize, and beautify your daily communication.
         </p>
       </div>
-      <AspectRatio ratio={16 / 9} className="w-full max-w-2xl">
-        <div className="bg-gradient-to-r from-gray-800 to-black rounded-md shadow-lg"></div>
-      </AspectRatio>
-    </div>
-  );
-};
-
-const Feature = ({ icon, title, description }) => {
-  return (
-    <div className="flex flex-col items-center p-4 border-r border-gray-700 last:border-r-0">
-      <div className="p-3 bg-white bg-opacity-10 rounded-full mb-3">
-        {icon}
-      </div>
-      <h3 className="text-lg font-semibold mb-2">{title}</h3>
-      <p className="text-gray-300 text-center">{description}</p>
-    </div>
-  );
-};
-
-const FeaturesSection = () => {
-  return (
-    <div className="bg-black bg-opacity-50 py-12">
-      <div className="max-w-6xl mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center text-white mb-8">
-          Everything You Need at Your Fingertips
-        </h2>
-        <div className="grid grid-cols-3 gap-8">
-          <Feature
-            icon={<Mail className="w-8 h-8 text-white opacity-70" />}
-            title="Unified Inbox"
-            description="Manage all your emails from multiple accounts in one single place."
-          />
-          <Feature
-            icon={<Settings className="w-8 h-8 text-white opacity-70" />}
-            title="Customizable Interface"
-            description="Tailor your email experience with customizable settings and themes."
-          />
-          <Feature
-            icon={<Star className="w-8 h-8 text-white opacity-70" />}
-            title="Important First"
-            description="Prioritize your most important emails so you never miss a thing."
-          />
-          <Feature
-            icon={<InboxIcon className="w-8 h-8 text-white opacity-70" />}
-            title="Smart Organization"
-            description="Automatic filters and tags to keep your inbox tidy."
-          />
-          <Feature
-            icon={<Wifi className="w-8 h-8 text-white opacity-70" />}
-            title="Always Connected"
-            description="Stay updated with real-time notifications and sync."
-          />
-          <Feature
-            icon={<Clock className="w-8 h-8 text-white opacity-70" />}
-            title="Scheduled Send"
-            description="Write now, send later with scheduled email delivery."
-          />
-          <Feature
-            icon={<Edit className="w-8 h-8 text-white opacity-70" />}
-            title="Effortless Composing"
-            description="Compose emails with ease using our intuitive editor."
-          />
-          <Feature
-            icon={<Camera className="w-8 h-8 text-white opacity-70" />}
-            title="Multimedia Friendly"
-            description="Easily manage and send multimedia attachments."
-          />
-          <Feature
-            icon={<File className="w-8 h-8 text-white opacity-70" />}
-            title="Document Integration"
-            description="Seamlessly integrate with popular document management systems."
-          />
-        </div>
+      <div className="grid grid-cols-3 gap-8 p-8 border-t border-gray-800">
+        <FeatureCard
+          icon={<Mail className="w-8 h-8 text-opacity-70" />}
+          title="Unified Inbox"
+          description="Manage all your emails from multiple accounts in one convenient location."
+        />
+        <FeatureCard
+          icon={<Star className="w-8 h-8 text-opacity-70" />}
+          title="Important First"
+          description="Automatically prioritize your most important messages with our smart filters."
+        />
+        <FeatureCard
+          icon={<Wifi className="w-8 h-8 text-opacity-70" />}
+          title="Always Connected"
+          description="Stay up to date with real-time notifications and continuous sync."
+        />
+        <FeatureCard
+          icon={<Clock className="w-8 h-8 text-opacity-70" />}
+          title="Snooze Emails"
+          description="Clear out distractions by snoozing emails until you're ready to handle them."
+        />
+        <FeatureCard
+          icon={<Settings className="w-8 h-8 text-opacity-70" />}
+          title="Customizable"
+          description="Tailor your email experience with customizable swipes, views, and more."
+        />
+        <FeatureCard
+          icon={<InboxIcon className="w-8 h-8 text-opacity-70" />}
+          title="Smart Search"
+          description="Find exactly what you're looking for with our powerful search and filtering tools."
+        />
       </div>
     </div>
   );
-};
+}
 
-const EmailClientComponent = () => {
+function FeatureCard({ icon, title, description }) {
   return (
-    <div className="flex flex-col bg-gray-900 text-white">
-      <HeroSection />
-      <div className="my-12">
-        <FeaturesSection />
-      </div>
-    </div>
+    <Card className="bg-transparent border border-gray-700 rounded-lg p-4">
+      <CardContent className="flex flex-col items-center">
+        <Badge variant="outline" className="p-3 rounded-full border border-gray-700 mb-4">
+          {icon}
+        </Badge>
+        <CardTitle className="text-lg font-semibold mb-2">{title}</CardTitle>
+        <CardDescription className="text-center">{description}</CardDescription>
+      </CardContent>
+    </Card>
   );
-};
-
-export default EmailClientComponent;
+}
