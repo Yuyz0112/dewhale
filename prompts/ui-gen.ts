9785b6047e98ee4bc5c4f08fa19200e214f46d55
Yanzhen Yu <yanzhen@smartx.com>
2023-12-21 11:26:57 +0800

    dump issue info

--- prompts/ui-gen.ts
+++ prompts/ui-gen.ts
@@ -1,12 +1,12 @@
-console.log(Deno.env.toObject());
-
 const GITHUB_EVENT_PATH = Deno.env.get("GITHUB_EVENT_PATH");
 if (!GITHUB_EVENT_PATH) {
   throw new Error("failed to get github event path");
 }
 
-const githubEvent = await import(GITHUB_EVENT_PATH, {
-  with: { type: "json" },
-});
+const githubEvent: { issue: { number: number; labels: string[] } } = (
+  await import(GITHUB_EVENT_PATH, {
+    with: { type: "json" },
+  })
+).default;
 
-console.log(githubEvent);
+console.log(githubEvent.issue);
