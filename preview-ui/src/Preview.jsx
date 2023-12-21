2571f5843ac6d6c2468a92a7f7dd4eb4e1b460c5
yz-yu <aryu0112@gmail.com>
2023-12-21 17:12:26 +0800

    [vx.dev] prompt: {   "prompt": "\r\n\r\n一个写实风格的计算器 UI\n",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/13651389/827e4c28-66c1-4ba1-b36d-c261a3e44871"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,1 +1,44 @@
-export default function VxDev() { return <p>vx.dev placeholder</p>; }
+import React from 'react';
+
+const CalculatorUI = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="bg-white p-4 shadow-lg rounded-lg">
+        <div className="bg-gray-200 p-2 rounded text-right">
+          <div className="text-gray-600 text-sm">Iddi</div>
+          <div className="text-gray-800 text-2xl">192.6</div>
+        </div>
+        <div className="grid grid-cols-4 gap-2 mt-4">
+          {['7', '8', '9', '×'].map((text) => (
+            <button key={text} className="bg-gray-300 p-4 rounded text-xl focus:outline-none">
+              {text}
+            </button>
+          ))}
+          <button className="bg-red-500 p-4 rounded text-xl text-white focus:outline-none">
+            C
+          </button>
+          {['4', '5', '6', '-'].map((text) => (
+            <button key={text} className="bg-gray-300 p-4 rounded text-xl focus:outline-none">
+              {text}
+            </button>
+          ))}
+          <button className="bg-gray-300 p-4 rounded text-xl focus:outline-none">M</button>
+          {['1', '2', '3', '+'].map((text) => (
+            <button key={text} className="bg-gray-300 p-4 rounded text-xl focus:outline-none">
+              {text}
+            </button>
+          ))}
+          <button className="bg-gray-300 p-4 rounded text-xl focus:outline-none">√</button>
+          <button className="bg-gray-300 p-4 rounded text-xl focus:outline-none">0</button>
+          <button className="bg-gray-300 p-4 rounded text-xl focus:outline-none">%</button>
+          <button className="bg-gray-300 p-4 rounded text-xl focus:outline-none">+</button>
+          <button className="col-span-2 bg-blue-500 p-4 rounded text-xl text-white focus:outline-none">
+            =
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalculatorUI;
