149a310310bf97b2e5c554ec208f85352eff5399
Yanzhen Yu <yanzhen@smartx.com>
2023-12-21 11:22:44 +0800

    read github event from path

--- prompts/ui-gen.ts
+++ prompts/ui-gen.ts
@@ -1,1 +1,12 @@
 console.log(Deno.env.toObject());
+
+const GITHUB_EVENT_PATH = Deno.env.get("GITHUB_EVENT_PATH");
+if (!GITHUB_EVENT_PATH) {
+  throw new Error("failed to get github event path");
+}
+
+const githubEvent = await import(GITHUB_EVENT_PATH, {
+  with: { type: "json" },
+});
+
+console.log(githubEvent);
