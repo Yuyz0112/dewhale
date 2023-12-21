84fa47fffa36cc6a6caefa7dd372c9cccda7de5a
yz-yu <aryu0112@gmail.com>
2023-12-21 17:18:36 +0800

    [vx.dev] prompt: {   "prompt": "\r\n\r\nRetirement goals calculator\nbuild error from your previous code\r\n\r\n```\r\n\"Tshirt\" is not exported by \"node_modules/lucide-react/dist/esm/lucide-react.js\"\r\n```\nanother build error\r\n```\r\nCould not load /opt/buildhome/repo/src/components/ui\r\n```",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/13651389/cda6f7bc-95c4-48a0-ae6a-e52e6b38f4ad"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,78 +1,72 @@
 import React from 'react';
-import { Heart, Home, Car, Medical, Edit, Trash, Plus, Minus } from 'lucide-react';
-import { Button, Slider, Input, Label } from "@/components/ui";
+import { ArrowRight, Home, Car, Heart, Camera, Edit, Trash, Plus, Minus } from 'lucide-react';
+import { Button, Input, Slider } from "@/components/ui";
+import { Label } from "@/components/ui/label";
 
-const RetirementGoalsCalculator = () => {
+export default function RetirementGoalsCalculator() {
   return (
     <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center p-4">
-      <header className="text-red-600 text-3xl font-bold mb-8">HSBC</header>
-      <div className="w-full max-w-4xl bg-gray-800 rounded-lg p-8">
-        <h2 className="text-2xl font-semibold mb-4">Add your dream retirement goals</h2>
-        <p className="text-gray-400 mb-8">What dream retirement goals would you like to include?</p>
-        <div className="grid grid-cols-4 gap-4 mb-8">
-          <div className="flex flex-col items-center justify-center p-4 bg-gray-700 rounded">
-            <Heart className="w-8 h-8 mb-2" />
-            <span>Travel</span>
-          </div>
-          <div className="flex flex-col items-center justify-center p-4 bg-gray-700 rounded">
-            <Home className="w-8 h-8 mb-2" />
+      <header className="text-2xl font-bold mb-8">
+        <span className="text-red-500">HSBC</span> 🏦
+      </header>
+      <main className="bg-gray-800 p-8 rounded-lg w-full max-w-4xl">
+        <h2 className="text-3xl font-bold mb-6">What dream retirement goals would you like to include?</h2>
+        <div className="grid grid-cols-3 gap-4 mb-8">
+          <Button variant="outline" className="flex flex-col items-center justify-center p-4">
+            <Home className="mb-2" />
             <span>Rental</span>
-          </div>
-          <div className="flex flex-col items-center justify-center p-4 bg-gray-700 rounded">
-            <Car className="w-8 h-8 mb-2" />
+          </Button>
+          <Button variant="outline" className="flex flex-col items-center justify-center p-4">
+            <Car className="mb-2" />
             <span>Car</span>
-          </div>
-          <div className="flex flex-col items-center justify-center p-4 bg-gray-700 rounded">
-            <Medical className="w-8 h-8 mb-2" />
+          </Button>
+          <Button variant="outline" className="flex flex-col items-center justify-center p-4">
+            <Heart className="mb-2" />
             <span>Medical</span>
-          </div>
-          {/* Additional goal icons can be added here */}
-        </div>
-        <div className="bg-gray-700 p-4 rounded-lg flex items-center justify-between mb-4">
-          <div className="flex items-center">
-            <Heart className="w-6 h-6 mr-2" />
-            <span className="font-semibold">Travel</span>
-          </div>
-          <Button variant="ghost" className="text-red-600">
-            <Trash className="w-5 h-5" />
+          </Button>
+          <Button variant="outline" className="flex flex-col items-center justify-center p-4">
+            <Camera className="mb-2" />
+            <span>Travel</span>
+          </Button>
+          <Button variant="outline" className="flex flex-col items-center justify-center p-4">
+            <Edit className="mb-2" />
+            <span>Legacy</span>
+          </Button>
+          <Button variant="outline" className="flex flex-col items-center justify-center p-4">
+            <Trash className="mb-2" />
+            <span>Clothes</span>
           </Button>
         </div>
-        <div className="flex items-center justify-between mb-4">
-          <Label className="flex-1 mr-4">
-            Cost
-            <Input className="mt-1" placeholder="20,000" />
-          </Label>
-          <Label className="flex-1">
-            Frequency
-            <Input className="mt-1" placeholder="Every 2 years" />
-          </Label>
-        </div>
-        <div className="flex items-center mb-8">
-          <Label className="flex-1 mr-4">
-            Age period
-            <Slider className="mt-1" defaultValue={[65, 89]} />
-          </Label>
+        <div className="bg-gray-700 p-4 rounded-lg mb-6">
+          <div className="flex items-center justify-between mb-4">
+            <h3 className="text-xl font-semibold">Your dream retirement goals</h3>
+            <span className="text-red-500">1 goal</span>
+          </div>
+          <div className="bg-gray-600 p-4 rounded-lg flex items-center justify-between">
+            <div className="flex items-center">
+              <Plus className="text-red-500 mr-2" />
+              <span className="font-semibold">Travel</span>
+            </div>
+            <div className="flex items-center">
+              <Label htmlFor="cost">Cost</Label>
+              <Input id="cost" defaultValue="20000" className="mx-2" />
+              <Label htmlFor="frequency">Frequency</Label>
+              <Select id="frequency" defaultValue="Every 2 years" className="mx-2">
+                <option>Every year</option>
+                <option>Every 2 years</option>
+                <option>Every 5 years</option>
+              </Select>
+              <Label htmlFor="age-range">Age period</Label>
+              <Slider id="age-range" defaultValue={[65, 89]} min={50} max={100} className="mx-2" />
+              <Minus className="text-red-500 ml-2" />
+            </div>
+          </div>
         </div>
         <div className="flex justify-between">
-          <Button variant="outline" className="text-gray-400 border-gray-600">
-            Previous
-          </Button>
-          <Button className="bg-red-600">
-            Next
-          </Button>
+          <Button variant="outline">Previous</Button>
+          <Button>Next</Button>
         </div>
-      </div>
-      <div className="flex justify-between items-center w-full max-w-4xl mt-4">
-        <div className="flex items-center">
-          <div className="w-2 h-2 bg-gray-400 rounded-full mr-2"></div>
-          <div className="w-2 h-2 bg-gray-400 rounded-full mr-2"></div>
-          <div className="w-2 h-2 bg-red-600 rounded-full mr-2"></div>
-          <div className="w-2 h-2 bg-gray-400 rounded-full"></div>
-        </div>
-        <span className="text-gray-400">Step 03/04</span>
-      </div>
+      </main>
     </div>
   );
-};
-
-export default RetirementGoalsCalculator;
+}
