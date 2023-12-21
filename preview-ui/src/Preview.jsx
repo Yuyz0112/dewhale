e04873112aaaa9253d315aded11e81c201f109df
yz-yu <aryu0112@gmail.com>
2023-12-21 16:15:19 +0800

    [vx.dev] prompt: {   "prompt": "\r\n\r\na team member card, black and white\ngot an error from your previous code: \r\n```\r\n[vite:load-fallback] Could not load /opt/buildhome/repo/src/components/ui\r\n```\nshorten the role select component in the card",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/13651389/1223755a-2bfc-4b7b-8712-c4b64812759a"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,58 +1,64 @@
-import React from 'react';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
-const TeamMemberCard = () => {
+export default function TeamMemberCard() {
   return (
-    <div className="bg-white rounded-lg p-4 shadow-md max-w-md mx-auto">
-      <h2 className="text-lg font-semibold">Team Members</h2>
-      <p className="text-sm text-gray-600 mb-4">Invite your team members to collaborate.</p>
-      <div className="space-y-4">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center space-x-3">
-            <Avatar>
-              <AvatarImage src="https://images.unsplash.com/photo-1564564295391-7f24f26f568b?w=250&dpr=2&q=80" alt="Sofia Davis" />
-              <AvatarFallback>SD</AvatarFallback>
-            </Avatar>
-            <div>
-              <div className="font-medium text-gray-800">Sofia Davis</div>
-              <div className="text-sm text-gray-500">m@example.com</div>
+    <div className="bg-white text-black p-6 rounded-lg shadow-md max-w-md mx-auto">
+      <Card>
+        <CardHeader>
+          <CardTitle>Team Members</CardTitle>
+          <CardDescription>Invite your team members to collaborate.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="space-y-4">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center space-x-3">
+                <Avatar>
+                  <AvatarImage src="https://images.unsplash.com/photo-1607746882042-944635dfe10e?w=800&dpr=2&q=80" alt="Sofia Davis" />
+                  <AvatarFallback>SD</AvatarFallback>
+                </Avatar>
+                <div>
+                  <div>Sofia Davis</div>
+                  <div className="text-sm text-gray-500">m@example.com</div>
+                </div>
+              </div>
+              <Select>
+                <SelectTrigger className="w-24">
+                  <SelectValue placeholder="Role" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="owner">Owner</SelectItem>
+                  <SelectItem value="admin">Admin</SelectItem>
+                  <SelectItem value="member">Member</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center space-x-3">
+                <Avatar>
+                  <AvatarImage src="https://images.unsplash.com/photo-1605462863863-10d9e47e15ee?w=800&dpr=2&q=80" alt="Jackson Lee" />
+                  <AvatarFallback>JL</AvatarFallback>
+                </Avatar>
+                <div>
+                  <div>Jackson Lee</div>
+                  <div className="text-sm text-gray-500">p@example.com</div>
+                </div>
+              </div>
+              <Select>
+                <SelectTrigger className="w-24">
+                  <SelectValue placeholder="Role" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="owner">Owner</SelectItem>
+                  <SelectItem value="admin">Admin</SelectItem>
+                  <SelectItem value="member">Member</SelectItem>
+                </SelectContent>
+              </Select>
             </div>
           </div>
-          <Select>
-            <SelectTrigger className="border-gray-300">
-              <SelectValue placeholder="Owner" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="owner">Owner</SelectItem>
-              <SelectItem value="member">Member</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="flex items-center justify-between">
-          <div className="flex items-center space-x-3">
-            <Avatar>
-              <AvatarImage src="https://images.unsplash.com/photo-1564564295391-7f24f26f568b?w=250&dpr=2&q=80" alt="Jackson Lee" />
-              <AvatarFallback>JL</AvatarFallback>
-            </Avatar>
-            <div>
-              <div className="font-medium text-gray-800">Jackson Lee</div>
-              <div className="text-sm text-gray-500">p@example.com</div>
-            </div>
-          </div>
-          <Select>
-            <SelectTrigger className="border-gray-300">
-              <SelectValue placeholder="Member" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="owner">Owner</SelectItem>
-              <SelectItem value="member">Member</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-      </div>
+        </CardContent>
+      </Card>
     </div>
   );
-};
-
-export default TeamMemberCard;
+}
