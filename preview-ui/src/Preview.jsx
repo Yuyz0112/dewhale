43a70a114a4c93b14d2e3ef12783b4b607c9c8ae
yz-yu <aryu0112@gmail.com>
2023-12-21 22:37:17 +0800

    [vx.dev] prompt: {   "prompt": "Build a Web-based file manager \r\n\r\n Clean, modern user interface using UI with light and dark modes.\r\n Optimistic, instant UI updates + loading states with  query.\r\n Drag n‘ drop local + remote uploads.\r\nSupport for individual files and folders\r\nSupports multiple file formats and displays icons according to different types.\r\nSupport for compressing, decompressing, moving and copying files\r\n\r\n\r\n\r\n\n存在运行时错误，重新调整保证正确性\nTableHeader 的类名不正确，请调整为与 TableCell 可以对应。\ntry again",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/18740395/85f7e24e-3ebe-4e2b-bde8-e4f1c222f7af",     "https://github.com/Yuyz0112/vx.dev/assets/18740395/d16c3e56-d247-403f-ae6b-1d5c2c01332a"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,135 +1,126 @@
-import { useState } from 'react';
-import { Folder, File, FileText, Image, Music, Video, Download, Upload, Plus, Minus, Trash, Settings, ArrowRight, Home } from 'lucide-react';
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import React from 'react';
+import { Folder, File, Upload, Plus, Minus, Download, Edit, Trash, Image, Video, Music, FileText, Archive, ChevronDown, Sun, Moon } from 'lucide-react';
+import { Table, TableBody, TableCaption, TableCell, TableHeader, TableHead, TableRow } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { Switch } from "@/components/ui/switch";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 
 export default function FileManager() {
-  const [darkMode, setDarkMode] = useState(false);
+  // This data would typically be fetched from an API, but is hardcoded here for the static prototype.
+  const files = [
+    { name: 'Directory', size: '64 B', modified: 'Nov 22, 2023 06:26 AM', created: 'Nov 22, 2023 06:26 AM', type: 'folder' },
+    { name: 'Facebook_Lite_381.0.0.3.100_apkcombo.com.apk', size: '2.22 MiB', modified: 'Nov 11, 2023 04:38 PM', created: 'Nov 22, 2023 06:22 AM', type: 'apk' },
+    { name: 'WhatsApp_Business_2.23.24.18_apkcombo.com.apk', size: '0 B', modified: 'Nov 11, 2023 04:26 PM', created: 'Nov 22, 2023 06:22 AM', type: 'apk' },
+  ];
 
-  const fileIcon = (extension) => {
-    switch (extension) {
-      case 'png':
-      case 'jpg':
-      case 'jpeg':
-      case 'gif':
-        return <Image />;
-      case 'mp3':
-      case 'wav':
-        return <Music />;
-      case 'mp4':
-      case 'avi':
-        return <Video />;
-      case 'txt':
-      case 'doc':
-      case 'docx':
-        return <FileText />;
-      default:
-        return <File />;
-    }
+  // File type to icon mapping
+  const fileTypeIcons = {
+    folder: <Folder />,
+    apk: <File />,
+    image: <Image />,
+    video: <Video />,
+    audio: <Music />,
+    document: <FileText />,
+    archive: <Archive />,
+    // Add more file type mappings here
   };
 
   return (
-    <div className={`flex h-screen ${darkMode ? 'bg-gray-800' : 'bg-gray-100'}`}>
-      <div className={`bg-white w-64 p-6 hidden sm:block ${darkMode ? 'bg-gray-900 text-white' : ''}`}>
-        <div className="flex items-center space-x-2 mb-6">
-          <Home className="w-6 h-6" />
-          <span className="font-bold text-lg">Turbo Folder</span>
+    <div className="flex flex-col h-screen bg-gray-100 dark:bg-gray-800">
+      <header className="flex justify-between items-center p-4 bg-white dark:bg-gray-900 shadow-md">
+        <h1 className="text-xl font-semibold text-gray-900 dark:text-white">Turbo Folder</h1>
+        <div className="flex items-center">
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button variant="outline" className="mr-2">
+                <Sun />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>Light Mode</TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button variant="outline">
+                <Moon />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>Dark Mode</TooltipContent>
+          </Tooltip>
         </div>
-        <div className="space-y-2">
-          <Button variant="ghost" className="w-full justify-start">
-            <Folder className="w-5 h-5 mr-2" /> My Drive
-          </Button>
-          <Button variant="ghost" className="w-full justify-start">
-            <Trash className="w-5 h-5 mr-2" /> Trash
-          </Button>
-          <Button variant="ghost" className="w-full justify-start">
-            <User className="w-5 h-5 mr-2" /> Users
-          </Button>
-          <Button variant="ghost" className="w-full justify-start">
-            <Settings className="w-5 h-5 mr-2" /> Logs
-          </Button>
+      </header>
+      <div className="flex flex-1 overflow-hidden">
+        <div className="bg-white dark:bg-gray-900 w-64 p-6 hidden sm:block">
+          <nav>
+            <ul>
+              <li className="mb-2"><Button asChild><a href="#">My Drive</a></Button></li>
+              <li className="mb-2"><Button asChild><a href="#">Trash</a></Button></li>
+              <li className="mb-2"><Button asChild><a href="#">Users</a></Button></li>
+              <li className="mb-2"><Button asChild><a href="#">Logs</a></Button></li>
+            </ul>
+          </nav>
         </div>
+        <main className="flex-1 overflow-y-auto p-4">
+          <Table>
+            <TableCaption className="text-left text-sm font-semibold text-gray-900 dark:text-white mb-4">My Drive</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[50px]"></TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Size</TableHead>
+                <TableHead>Last modified</TableHead>
+                <TableHead>Created at</TableHead>
+                <TableHead className="w-[150px] text-right">Actions</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {files.map((file, index) => (
+                <TableRow key={index}>
+                  <TableCell>{fileTypeIcons[file.type] || <File />}</TableCell>
+                  <TableCell>{file.name}</TableCell>
+                  <TableCell>{file.size}</TableCell>
+                  <TableCell>{file.modified}</TableCell>
+                  <TableCell>{file.created}</TableCell>
+                  <TableCell className="text-right">
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button variant="outline" aria-label="Options">
+                          <ChevronDown />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        <DropdownMenuItem>Open</DropdownMenuItem>
+                        <DropdownMenuItem>Download</DropdownMenuItem>
+                        <DropdownMenuItem>Share</DropdownMenuItem>
+                        <DropdownMenuItem>Move</DropdownMenuItem>
+                        <DropdownMenuItem>Copy</DropdownMenuItem>
+                        <DropdownMenuItem>Compress</DropdownMenuItem>
+                        <DropdownMenuItem>Extract</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </main>
       </div>
-      <div className="flex-1 p-6">
-        <div className="flex justify-between items-center mb-4">
-          <h1 className="text-xl font-semibold">My Drive</h1>
-          <div className="flex space-x-2">
-            <Button variant="solid" className="flex items-center">
-              <Upload className="w-4 h-4 mr-2" /> Upload
-            </Button>
-            <Button variant="solid" className="flex items-center">
-              <Plus className="w-4 h-4 mr-2" /> New folder
-            </Button>
-            <Button variant="solid" className="flex items-center">
-              <File className="w-4 h-4 mr-2" /> New file
-            </Button>
-          </div>
+      <footer className="flex justify-between items-center p-4 bg-white dark:bg-gray-900 shadow-md">
+        <div>
+          <Button variant="outline" className="mr-2">
+            <Upload className="mr-1" /> Upload
+          </Button>
+          <Button variant="outline" className="mr-2">
+            <Plus className="mr-1" /> New folder
+          </Button>
+          <Button variant="outline">
+            <File className="mr-1" /> New file
+          </Button>
         </div>
-        <Table>
-          <TableCaption>Total 3 items in the current directory.</TableCaption>
-          <TableHead>
-            <TableRow>
-              <TableHeader className="w-[50px]"></TableHeader>
-              <TableHeader>Name</TableHeader>
-              <TableHeader>Size</TableHeader>
-              <TableHeader>Last modified</TableHeader>
-              <TableHeader>Created at</TableHeader>
-              <TableHeader className="w-[150px]"></TableHeader>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {/* Example file row */}
-            <TableRow>
-              <TableCell>
-                <Checkbox />
-              </TableCell>
-              <TableCell className="flex items-center">
-                {fileIcon('png')}
-                <span className="ml-2">example_image.png</span>
-              </TableCell>
-              <TableCell>2.2 MB</TableCell>
-              <TableCell>Apr 10, 2023</TableCell>
-              <TableCell>Apr 10, 2023</TableCell>
-              <TableCell>
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost">Actions</Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent>
-                    <DropdownMenuItem>Open</DropdownMenuItem>
-                    <DropdownMenuItem>Download</DropdownMenuItem>
-                    <DropdownMenuItem>Share</DropdownMenuItem>
-                    <DropdownMenuItem>Move</DropdownMenuItem>
-                    <DropdownMenuItem>Copy</DropdownMenuItem>
-                    <DropdownMenuItem>Compress</DropdownMenuItem>
-                    <DropdownMenuItem>Extract</DropdownMenuItem>
-                    <DropdownMenuItem>Delete</DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
-            </TableRow>
-            {/* More file rows... */}
-          </TableBody>
-        </Table>
-      </div>
-      <footer className={`bg-white p-4 shadow-md ${darkMode ? 'bg-gray-900 text-white' : ''}`}>
-        <div className="flex justify-between items-center">
-          <div className="flex items-center">
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button variant="ghost" onClick={() => setDarkMode(!darkMode)}>
-                  <Settings className="w-5 h-5" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>Theme</TooltipContent>
-            </Tooltip>
-            <Switch checked={darkMode} onCheckedChange={setDarkMode} />
-          </div>
-          <Button variant="ghost">Sign out</Button>
+        <div>
+          <Switch id="theme-toggle" className="mr-2" />
+          <label htmlFor="theme-toggle" className="text-sm font-medium text-gray-900 dark:text-white">Dark Mode</label>
         </div>
       </footer>
     </div>
