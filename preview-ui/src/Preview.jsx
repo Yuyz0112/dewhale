aeed3b2e6ae33949fe50806a36ac05ba542fed8b
yz-yu <aryu0112@gmail.com>
2023-12-21 18:43:51 +0800

    [vx.dev] prompt: {   "prompt": "An ecommerce dashboard with a sidebar navigation and a table of recent orders.\nyour previous code has a runtime error: `FocusGroupItem` must be used within `NavigationMenu`",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,6 +1,13 @@
 import { useState } from 'react';
-import { Home, User, Settings, ShoppingBag, CreditCard, Package, BarChart2, FileText, LogOut } from 'lucide-react';
+import { Home, User, Settings, Bell, File, Calendar, Printer } from 'lucide-react';
 import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuTrigger,
+  NavigationMenuContent,
+  NavigationMenuViewport,
   Table,
   TableBody,
   TableCaption,
@@ -8,86 +15,107 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table";
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, NavigationMenuViewport } from "@/components/ui/navigation-menu";
+} from '@/components/ui';
 
 export default function EcommerceDashboard() {
-  const [selectedMenu, setSelectedMenu] = useState('dashboard');
+  const [selectedMenuItem, setSelectedMenuItem] = useState('dashboard');
 
   return (
     <div className="flex h-screen bg-gray-100">
-      <aside className="w-64 bg-white p-6 hidden sm:block">
+      <aside className="w-64 bg-white p-4">
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <NavigationMenuLink className={selectedMenu === 'dashboard' ? 'text-blue-500' : ''} onClick={() => setSelectedMenu('dashboard')}>
-                <Home className="inline-block mr-2" /> Dashboard
+              <NavigationMenuLink
+                as="button"
+                className={`flex items-center p-2 rounded-md hover:bg-gray-200 ${
+                  selectedMenuItem === 'dashboard' ? 'bg-gray-200' : ''
+                }`}
+                onClick={() => setSelectedMenuItem('dashboard')}
+              >
+                <Home className="mr-2" />
+                Dashboard
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink className={selectedMenu === 'orders' ? 'text-blue-500' : ''} onClick={() => setSelectedMenu('orders')}>
-                <ShoppingBag className="inline-block mr-2" /> Orders
+              <NavigationMenuLink
+                as="button"
+                className={`flex items-center p-2 rounded-md hover:bg-gray-200 ${
+                  selectedMenuItem === 'orders' ? 'bg-gray-200' : ''
+                }`}
+                onClick={() => setSelectedMenuItem('orders')}
+              >
+                <File className="mr-2" />
+                Orders
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink className={selectedMenu === 'products' ? 'text-blue-500' : ''} onClick={() => setSelectedMenu('products')}>
-                <Package className="inline-block mr-2" /> Products
+              <NavigationMenuLink
+                as="button"
+                className={`flex items-center p-2 rounded-md hover:bg-gray-200 ${
+                  selectedMenuItem === 'calendar' ? 'bg-gray-200' : ''
+                }`}
+                onClick={() => setSelectedMenuItem('calendar')}
+              >
+                <Calendar className="mr-2" />
+                Calendar
               </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuLink className={selectedMenu === 'customers' ? 'text-blue-500' : ''} onClick={() => setSelectedMenu('customers')}>
-                <User className="inline-block mr-2" /> Customers
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink className={selectedMenu === 'reports' ? 'text-blue-500' : ''} onClick={() => setSelectedMenu('reports')}>
-                <BarChart2 className="inline-block mr-2" /> Reports
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink className={selectedMenu === 'integrations' ? 'text-blue-500' : ''} onClick={() => setSelectedMenu('integrations')}>
-                <Settings className="inline-block mr-2" /> Integrations
+              <NavigationMenuLink
+                as="button"
+                className={`flex items-center p-2 rounded-md hover:bg-gray-200 ${
+                  selectedMenuItem === 'settings' ? 'bg-gray-200' : ''
+                }`}
+                onClick={() => setSelectedMenuItem('settings')}
+              >
+                <Settings className="mr-2" />
+                Settings
               </NavigationMenuLink>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
-        <div className="mt-6">
-          <NavigationMenuLink>
-            <LogOut className="inline-block mr-2" /> Logout
-          </NavigationMenuLink>
-        </div>
       </aside>
       <main className="flex-1 p-6">
         <div className="bg-white rounded shadow p-6">
-          <h1 className="text-xl font-bold mb-4">Recent Orders</h1>
           <Table>
-            <TableCaption>A list of your recent orders.</TableCaption>
-            <TableHead>
+            <TableCaption>Recent Orders</TableCaption>
+            <TableHeader>
               <TableRow>
-                <TableHead className="w-[100px]">Order ID</TableHead>
+                <TableHead>Order ID</TableHead>
                 <TableHead>Date</TableHead>
                 <TableHead>Status</TableHead>
-                <TableHead className="text-right">Total</TableHead>
+                <TableHead>Total</TableHead>
+                <TableHead>Actions</TableHead>
               </TableRow>
-            </TableHead>
+            </TableHeader>
             <TableBody>
               <TableRow>
-                <TableCell className="font-medium">ORD001</TableCell>
-                <TableCell>2023-03-15</TableCell>
+                <TableCell>#001</TableCell>
+                <TableCell>2023-03-10</TableCell>
                 <TableCell>Shipped</TableCell>
-                <TableCell className="text-right">$199.99</TableCell>
+                <TableCell>$250.00</TableCell>
+                <TableCell>
+                  <Printer className="cursor-pointer hover:text-blue-500" />
+                </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="font-medium">ORD002</TableCell>
-                <TableCell>2023-03-14</TableCell>
+                <TableCell>#002</TableCell>
+                <TableCell>2023-03-11</TableCell>
                 <TableCell>Processing</TableCell>
-                <TableCell className="text-right">$299.99</TableCell>
+                <TableCell>$120.00</TableCell>
+                <TableCell>
+                  <Printer className="cursor-pointer hover:text-blue-500" />
+                </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell className="font-medium">ORD003</TableCell>
-                <TableCell>2023-03-13</TableCell>
+                <TableCell>#003</TableCell>
+                <TableCell>2023-03-12</TableCell>
                 <TableCell>Delivered</TableCell>
-                <TableCell className="text-right">$399.99</TableCell>
+                <TableCell>$180.00</TableCell>
+                <TableCell>
+                  <Printer className="cursor-pointer hover:text-blue-500" />
+                </TableCell>
               </TableRow>
             </TableBody>
           </Table>
