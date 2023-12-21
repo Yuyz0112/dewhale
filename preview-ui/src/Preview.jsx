5dddab9b4874050d4d13ccf14a5e77d0ef6d1d3b
yz-yu <aryu0112@gmail.com>
2023-12-21 15:47:14 +0800

    [vx.dev] prompt: {   "prompt": "a weather app looks like the iOS weather app\r\nmake the background a blue gradient\nremove the bar chart, the inner content should have width 500px\n\"WeatherIcon\" is not exported by \"node_modules/lucide-react/dist/esm/lucide-react.js\",",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,69 +1,80 @@
-import React from 'react';
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { WeatherIcon } from "lucide-react";
 import { ResponsiveLine } from "@nivo/line";
+import { Sun, CloudRain, Wind } from "lucide-react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function WeatherApp() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-blue-400 to-blue-600">
       <div className="flex justify-center items-center min-h-screen">
-        <div className="w-[500px] bg-white bg-opacity-80 rounded-lg shadow-lg p-4">
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-2xl font-bold text-gray-800">San Francisco</CardTitle>
-              <CardDescription className="text-sm text-gray-600">CA, USA</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div className="flex items-center justify-between py-4">
-                <div className="flex items-center">
-                  <WeatherIcon className="w-12 h-12 text-blue-500" />
-                  <div className="ml-4">
-                    <div className="text-5xl font-semibold">72°</div>
-                    <div className="text-sm text-gray-600">Sunny</div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden" style={{ width: '500px' }}>
+          <Tabs defaultValue="today" className="w-full">
+            <TabsList>
+              <TabsTrigger value="today">Today</TabsTrigger>
+              <TabsTrigger value="week">This Week</TabsTrigger>
+            </TabsList>
+            <TabsContent value="today">
+              <Card>
+                <CardHeader>
+                  <CardTitle>San Francisco</CardTitle>
+                  <CardDescription>Today, 5:00 PM</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex justify-between items-center">
+                    <Sun className="w-24 h-24 text-yellow-400" />
+                    <div className="text-6xl">72°</div>
                   </div>
-                </div>
-                <div className="text-right">
-                  <div className="text-sm text-gray-600">H: 77° L: 56°</div>
-                  <div className="text-sm text-gray-600">10% chance of rain</div>
-                </div>
-              </div>
-              <AspectRatio ratio={16 / 9} className="hidden">
-                <div className="w-full h-full">
-                  <ResponsiveLine
-                    data={[
-                      {
-                        id: "temperature",
-                        data: [
-                          { x: "6 AM", y: 56 },
-                          { x: "9 AM", y: 60 },
-                          { x: "12 PM", y: 68 },
-                          { x: "3 PM", y: 72 },
-                          { x: "6 PM", y: 70 },
-                          { x: "9 PM", y: 65 },
-                        ],
-                      },
-                    ]}
-                    margin={{ top: 20, right: 20, bottom: 60, left: 80 }}
-                    xScale={{ type: "point" }}
-                    yScale={{ type: "linear", min: "auto", max: "auto" }}
-                    axisTop={null}
-                    axisRight={null}
-                    enableGridX={false}
-                    enableGridY={false}
-                    colors={{ scheme: "nivo" }}
-                    lineWidth={3}
-                    pointSize={10}
-                    pointColor={{ from: 'color', modifiers: [] }}
-                    pointBorderWidth={2}
-                    pointBorderColor={{ from: 'serieColor' }}
-                    useMesh={true}
-                    legends={[]}
-                  />
-                </div>
+                  <div className="flex justify-between items-center mt-4">
+                    <div className="flex items-center">
+                      <CloudRain className="w-6 h-6 text-blue-500 mr-2" />
+                      <span>10%</span>
+                    </div>
+                    <div className="flex items-center">
+                      <Wind className="w-6 h-6 text-gray-500 mr-2" />
+                      <span>6 mph</span>
+                    </div>
+                  </div>
+                </CardContent>
+              </Card>
+            </TabsContent>
+            <TabsContent value="week">
+              <AspectRatio ratio={16 / 9}>
+                <ResponsiveLine
+                  data={[
+                    {
+                      id: "temp",
+                      data: [
+                        { x: "Monday", y: 70 },
+                        { x: "Tuesday", y: 75 },
+                        { x: "Wednesday", y: 70 },
+                        { x: "Thursday", y: 80 },
+                        { x: "Friday", y: 78 },
+                        { x: "Saturday", y: 85 },
+                        { x: "Sunday", y: 90 },
+                      ],
+                    },
+                  ]}
+                  margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                  xScale={{ type: "point" }}
+                  yScale={{
+                    type: "linear",
+                    min: 0,
+                    max: "auto",
+                  }}
+                  axisTop={null}
+                  axisRight={null}
+                  enableGridX={false}
+                  enableGridY={false}
+                  colors={{ scheme: "nivo" }}
+                  enablePoints={false}
+                  enableCrosshair={false}
+                  useMesh={true}
+                  legends={[]}
+                />
               </AspectRatio>
-            </CardContent>
-          </Card>
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
     </div>
