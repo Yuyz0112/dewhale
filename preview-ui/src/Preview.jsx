255c53960b4a9559c80b5ff5a49a032f9328885b
yz-yu <aryu0112@gmail.com>
2023-12-21 15:50:33 +0800

    [vx.dev] prompt: {   "prompt": "\r\n\r\na team member card, black and white\n",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/13651389/1223755a-2bfc-4b7b-8712-c4b64812759a"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,1 +1,51 @@
-export default function VxDev() { return <p>vx.dev placeholder</p>; }
+import { Card, CardContent, Avatar, AvatarImage, AvatarFallback, Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui";
+import { User } from "lucide-react";
+
+export default function TeamMemberCard() {
+  return (
+    <Card className="max-w-md mx-auto bg-white rounded-lg overflow-hidden shadow-lg p-6">
+      <h2 className="text-lg font-semibold">Team Members</h2>
+      <p className="text-sm text-gray-600 mb-4">Invite your team members to collaborate.</p>
+      <CardContent>
+        <div className="flex items-center space-x-4 mb-3">
+          <Avatar>
+            <AvatarImage src="https://images.unsplash.com/photo-1607746882042-944635dfe10e?w=800&dpr=2&q=80" alt="Sofia Davis" />
+            <AvatarFallback delayMs={600}>SD</AvatarFallback>
+          </Avatar>
+          <div className="flex-1">
+            <div className="font-medium text-gray-800">Sofia Davis</div>
+            <div className="text-gray-500 text-sm">m@example.com</div>
+          </div>
+          <Select>
+            <SelectTrigger className="w-24">
+              <SelectValue placeholder="Owner" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="owner">Owner</SelectItem>
+              <SelectItem value="member">Member</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex items-center space-x-4">
+          <Avatar>
+            <AvatarImage src="https://images.unsplash.com/photo-1607746881559-7b8c535e29f9?w=800&dpr=2&q=80" alt="Jackson Lee" />
+            <AvatarFallback delayMs={600}>JL</AvatarFallback>
+          </Avatar>
+          <div className="flex-1">
+            <div className="font-medium text-gray-800">Jackson Lee</div>
+            <div className="text-gray-500 text-sm">p@example.com</div>
+          </div>
+          <Select>
+            <SelectTrigger className="w-24">
+              <SelectValue placeholder="Member" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="owner">Owner</SelectItem>
+              <SelectItem value="member">Member</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
