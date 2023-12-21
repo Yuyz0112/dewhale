491363c1c689d90c3c9a34f26a4e4c3887a85646
yz-yu <aryu0112@gmail.com>
2023-12-21 16:29:51 +0800

    [vx.dev] prompt: {   "prompt": "create a colorful pricing page, with three pricing cards\n\r\n\neach card has a colorful background, you can add some beautiful gradients to it",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/13651389/eb9205cc-ae65-4b30-bc03-7148983ef5d2"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,6 +1,5 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Slider } from "@/components/ui/slider";
 
 export default function PricingPage() {
@@ -9,67 +8,60 @@ export default function PricingPage() {
       <section className="text-center mb-12">
         <h1 className="text-4xl font-bold mb-4">Three plans, three prices.</h1>
         <p className="text-xl mb-8">No setup costs or any other hidden fees.</p>
-        <div className="mb-4">
+        <div className="inline-block">
           <label htmlFor="agenda-slider" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">How many agendas will you need?</label>
-          <Slider id="agenda-slider" defaultValue={[1]} max={10} step={1} />
+          <Slider id="agenda-slider" defaultValue={[1]} max={10} step={1} className="w-64 mb-2" />
+          <div className="text-sm">10 or more? Request a quote</div>
         </div>
-        <p className="text-sm">10 or more? Request a quote</p>
-      </section>
-
-      <section className="mb-12">
-        <ToggleGroup type="single" defaultValue="monthly" aria-label="Select billing type">
-          <ToggleGroupItem value="monthly" className="text-sm">Monthly billing</ToggleGroupItem>
-          <ToggleGroupItem value="yearly" className="text-sm">Yearly billing <span className="text-xs text-green-600 font-semibold ml-1">Save 15%</span></ToggleGroupItem>
-        </ToggleGroup>
       </section>
 
       <section>
-        <h2 className="text-2xl font-bold mb-6">Pick your plan</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <Card className="bg-white rounded-lg shadow-md p-4">
-            <CardHeader>
-              <CardTitle className="text-lg font-semibold text-blue-600">Base</CardTitle>
-              <CardDescription className="text-sm">Individual golf professional (&lt;500 clients)</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p className="text-sm mb-4">The perfect match for starting golf professionals and golf professionals with a smaller clientele.</p>
-              <p className="text-3xl font-bold mb-4">€11/mo</p>
-            </CardContent>
-            <CardFooter>
+        <Tabs defaultValue="monthly" className="mb-8">
+          <TabsList className="flex justify-center mb-4">
+            <TabsTrigger value="monthly" className="px-4 py-2">Monthly billing</TabsTrigger>
+            <TabsTrigger value="yearly" className="px-4 py-2">Yearly billing <span className="ml-2 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">Save 15%</span></TabsTrigger>
+          </TabsList>
+        </Tabs>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="rounded-lg shadow-lg overflow-hidden">
+            <div className="bg-gradient-to-r from-blue-400 to-blue-600 p-6 text-white">
+              <h2 className="text-2xl font-bold mb-4">Base</h2>
+              <p className="mb-4">Individual golf professional (&lt;500 clients)</p>
+              <p>The perfect match for starting golf professionals and golf professionals with a smaller clientele.</p>
+            </div>
+            <div className="bg-white p-6">
+              <div className="text-4xl font-bold mb-4">€11/mo</div>
               <Button variant="solid" className="w-full">Choose Base</Button>
-              <p className="text-xs text-gray-500 mt-2">Additional agenda €11 month</p>
-            </CardFooter>
-          </Card>
+              <p className="mt-4 text-sm text-gray-600">Additional agenda €11 month</p>
+            </div>
+          </div>
 
-          <Card className="bg-white rounded-lg shadow-md p-4">
-            <CardHeader>
-              <CardTitle className="text-lg font-semibold text-green-600">Plus</CardTitle>
-              <CardDescription className="text-sm">Individual golf professional (&gt;500 clients) &amp; Smaller academies (&lt;3 professionals)</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p className="text-sm mb-4">For the golf professionals who already have many teaching hours and who have a large customer database and for the small golf academies.</p>
-              <p className="text-3xl font-bold mb-4">€58/mo</p>
-            </CardContent>
-            <CardFooter>
+          <div className="rounded-lg shadow-lg overflow-hidden">
+            <div className="bg-gradient-to-r from-green-400 to-green-600 p-6 text-white">
+              <h2 className="text-2xl font-bold mb-4">Plus</h2>
+              <p className="mb-4">Individual golf professional (&gt;500 clients) &amp; Smaller academies (&lt;3 professionals)</p>
+              <p>For the golf professionals who already have many teaching hours and who have a large customer database and for the small golf academies.</p>
+            </div>
+            <div className="bg-white p-6">
+              <div className="text-4xl font-bold mb-4">€58/mo</div>
               <Button variant="solid" className="w-full">Choose Plus</Button>
-              <p className="text-xs text-gray-500 mt-2">Additional agenda €18 month</p>
-            </CardFooter>
-          </Card>
+              <p className="mt-4 text-sm text-gray-600">Additional agenda €18 month</p>
+            </div>
+          </div>
 
-          <Card className="bg-white rounded-lg shadow-md p-4">
-            <CardHeader>
-              <CardTitle className="text-lg font-semibold text-red-600">Premium</CardTitle>
-              <CardDescription className="text-sm">Larger academies (more than 3 professionals)</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p className="text-sm mb-4">The ultimate solution for the bigger academies with a larger clientele.</p>
-              <p className="text-3xl font-bold mb-4">€105/mo</p>
-            </CardContent>
-            <CardFooter>
+          <div className="rounded-lg shadow-lg overflow-hidden">
+            <div className="bg-gradient-to-r from-purple-400 to-purple-600 p-6 text-white">
+              <h2 className="text-2xl font-bold mb-4">Premium</h2>
+              <p className="mb-4">Larger academies (more than 3 professionals)</p>
+              <p>The ultimate solution for the bigger academies with a larger clientele.</p>
+            </div>
+            <div className="bg-white p-6">
+              <div className="text-4xl font-bold mb-4">€105/mo</div>
               <Button variant="solid" className="w-full">Choose Premium</Button>
-              <p className="text-xs text-gray-500 mt-2">Additional agenda €25 month</p>
-            </CardFooter>
-          </Card>
+              <p className="mt-4 text-sm text-gray-600">Additional agenda €25 month</p>
+            </div>
+          </div>
         </div>
       </section>
     </main>
