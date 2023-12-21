020c7c53c9ebdd62c00e4d6a676fc1b99ba9deda
yz-yu <aryu0112@gmail.com>
2023-12-21 18:14:06 +0800

    [vx.dev] prompt: {   "prompt": "\r\n\r\ncreate a review and rating page like this\nadd at least three comments in the prototype, make the share button look engaged, and make the delete button look danger\nmake sure all the icons are imported correctly",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/13651389/66f9de34-7aa9-4c36-b1e3-f8c7f4c1d96c"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,93 +1,90 @@
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Star } from "lucide-react";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { Share2, MessageCircle, Download, Trash2 } from "lucide-react";
 
 export default function ReviewAndRatingPage() {
-  const reviews = [
-    {
-      id: 1,
-      name: "Nina Holloway",
-      date: "29 Aug 2017",
-      comment: "So you're going abroad, you've chosen your destination and now you have to choose a hotel. Ten years ago, you'd have probably visited your local travel agent and trusted the face-to-face advice you were given by the so called 'experts'. Whether its a driving tour, a cruise or a bus, leaf viewing is a great way to spend",
-      rating: 5,
-      helpfulCount: 7,
-    },
-    {
-      id: 2,
-      name: "Steve Fletcher",
-      date: "30 Aug 2017",
-      comment: "Whether its a driving tour, a cruise or a bus, leaf viewing is a great way to spend a fall vacation. It's also big tour business and the are many options. As you dream of that hot apple cider on a crisp afternoon do a quick check list whether its a driving tour, a cruise or a bus, leaf viewing is a great way to spend a fall vacation. It's also big tour business.",
-      rating: 4,
-      helpfulCount: 6,
-    },
-    {
-      id: 3,
-      name: "Oscar Rogers",
-      date: "29 Aug 2017",
-      comment: "It's also big tour business and the are many options. As you dream of that hot apple cider on a crisp afternoon do a quick check list whether its a driving tour, a cruise or a bus, leaf viewing is a great way to spend a fall vacation. It's also big tour business and the are many options. As you dream of that hot apple cider on a crisp afternoon do a quick check list",
-      rating: 3,
-      helpfulCount: 2,
-    },
-  ];
-
   return (
-    <div className="p-4 bg-gray-100">
-      <div className="max-w-4xl mx-auto">
-        <h1 className="text-2xl font-semibold mb-6">Review and rating › Marshall Islands</h1>
-        <div className="bg-white rounded-lg shadow-lg p-6">
-          {reviews.map((review) => (
-            <Card key={review.id} className="mb-4">
-              <CardContent className="flex items-start space-x-4">
-                <Avatar>
-                  <AvatarImage src={`https://i.pravatar.cc/150?u=${review.id}`} alt={review.name} />
+    <div className="bg-gray-100 p-8">
+      <div className="max-w-4xl mx-auto bg-white rounded-lg shadow-lg p-6">
+        <header className="flex justify-between items-center mb-6">
+          <h1 className="text-xl font-semibold">Review and rating &gt; Marshall Islands</h1>
+          <div className="flex items-center">
+            <Avatar>
+              <AvatarImage src="https://images.unsplash.com/photo-1605462863863-10d9e47e15ee?w=800&dpr=2&q=80" />
+              <AvatarFallback>U</AvatarFallback>
+            </Avatar>
+            <span className="ml-2">George Kennedy</span>
+          </div>
+        </header>
+        <main>
+          {[
+            {
+              name: "Nina Holloway",
+              date: "29 Aug 2017",
+              comment: "So you're going abroad, you've chosen your destination and now you have to choose a hotel. Ten years ago, you'd have probably visited your local travel agent and trusted the face-to-face advice you were given by the so called 'experts'. Whether its a driving tour, a cruise or a bus, leaf viewing is a great way to spend",
+              rating: 5,
+              reactions: 7,
+            },
+            {
+              name: "Steve Fletcher",
+              date: "30 Aug 2017",
+              comment: "Whether its a driving tour, a cruise or a bus, leaf viewing is a great way to spend a fall vacation. It's also big tour business and the are many options. As you dream of that hot apple cider on a crisp afternoon do a quick check list whether its a driving tour, a cruise or a bus, leaf viewing is a great way to spend a fall vacation. It's also big tour business.",
+              rating: 4,
+              reactions: 6,
+            },
+            {
+              name: "Oscar Rogers",
+              date: "29 Aug 2017",
+              comment: "It's also big tour business and the are many options. As you dream of that hot apple cider on a crisp afternoon do a quick check list leaf viewing is a great way to spend a fall vacation. It's also big tour business and the are many options. As you dream of that hot apple cider on a crisp afternoon do a quick check list",
+              rating: 3,
+              reactions: 2,
+            },
+          ].map((review, index) => (
+            <Card key={index} className="mb-4">
+              <CardContent className="flex">
+                <Avatar className="mr-4">
+                  <AvatarImage src={`https://i.pravatar.cc/150?img=${index + 1}`} />
                   <AvatarFallback>{review.name.charAt(0)}</AvatarFallback>
                 </Avatar>
                 <div className="flex-1">
-                  <p className="text-sm font-semibold">{review.name}</p>
-                  <p className="text-xs text-gray-500">{review.date}</p>
-                  <div className="flex items-center mt-1">
-                    {[...Array(5)].map((_, index) => (
-                      <Star key={index} className={`w-4 h-4 ${index < review.rating ? 'text-yellow-400' : 'text-gray-300'}`} />
-                    ))}
+                  <div className="flex justify-between">
+                    <div>
+                      <div className="font-semibold">{review.name}</div>
+                      <div className="text-sm text-gray-500">{review.date}</div>
+                    </div>
+                    <div className="flex items-center">
+                      {[...Array(review.rating)].map((_, i) => (
+                        <Star key={i} className="text-yellow-400 w-5 h-5" />
+                      ))}
+                      <span className="ml-2 text-gray-500">{review.reactions}</span>
+                    </div>
                   </div>
-                  <p className="text-sm text-gray-600 mt-2">{review.comment}</p>
+                  <p className="mt-2 text-gray-700">{review.comment}</p>
                 </div>
               </CardContent>
-              <CardFooter className="flex items-center justify-between">
-                <Button variant="ghost" className="text-green-600">
-                  <Star className="w-4 h-4 mr-1" />
-                  {review.helpfulCount}
-                </Button>
-                <div className="flex space-x-2">
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button variant="ghost" className="text-yellow-600">
-                        <Share className="w-4 h-4" />
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent>Share Review</TooltipContent>
-                  </Tooltip>
-                  <Button variant="ghost" className="text-blue-600">
-                    <Reply className="w-4 h-4" />
-                  </Button>
-                  <Button variant="ghost" className="text-gray-600">
-                    <Download className="w-4 h-4" />
-                  </Button>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <Button variant="ghost" className="text-red-600">
-                        <Trash className="w-4 h-4" />
-                      </Button>
-                    </TooltipTrigger>
-                    <TooltipContent>Delete Review</TooltipContent>
-                  </Tooltip>
-                </div>
-              </CardFooter>
             </Card>
           ))}
-        </div>
+        </main>
+        <aside className="flex justify-end space-x-2">
+          <Button variant="outline" className="flex items-center space-x-2 bg-yellow-200 text-yellow-700 border-yellow-700">
+            <Share2 className="w-5 h-5" />
+            <span>Share Review</span>
+          </Button>
+          <Button variant="outline" className="flex items-center space-x-2 bg-green-200 text-green-700 border-green-700">
+            <MessageCircle className="w-5 h-5" />
+            <span>Respond</span>
+          </Button>
+          <Button variant="outline" className="flex items-center space-x-2">
+            <Download className="w-5 h-5" />
+            <span>Download</span>
+          </Button>
+          <Button variant="outline" className="flex items-center space-x-2 bg-red-200 text-red-700 border-red-700">
+            <Trash2 className="w-5 h-5" />
+            <span>Delete Review</span>
+          </Button>
+        </aside>
       </div>
     </div>
   );
