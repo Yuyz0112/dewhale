a1fca0bb4a6e8922aea3a36131ac1526b5688403
yz-yu <aryu0112@gmail.com>
2023-12-21 19:25:24 +0800

    [vx.dev] prompt: {   "prompt": "beautiful 4 card gradients \n",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,1 +1,34 @@
-export default function VxDev() { return <p>vx.dev placeholder</p>; }
+import React from 'react';
+
+const GradientCard = () => {
+  return (
+    <div className="p-8 space-y-4">
+      <div className="rounded-xl overflow-hidden shadow-lg">
+        <div className="bg-gradient-to-r from-purple-500 to-indigo-600 p-4 text-white">
+          <h2 className="text-xl font-bold">Gradient Card 1</h2>
+          <p>Here is some content for the first gradient card.</p>
+        </div>
+      </div>
+      <div className="rounded-xl overflow-hidden shadow-lg">
+        <div className="bg-gradient-to-r from-green-400 to-blue-500 p-4 text-white">
+          <h2 className="text-xl font-bold">Gradient Card 2</h2>
+          <p>Here is some content for the second gradient card.</p>
+        </div>
+      </div>
+      <div className="rounded-xl overflow-hidden shadow-lg">
+        <div className="bg-gradient-to-r from-orange-500 to-pink-500 p-4 text-white">
+          <h2 className="text-xl font-bold">Gradient Card 3</h2>
+          <p>Here is some content for the third gradient card.</p>
+        </div>
+      </div>
+      <div className="rounded-xl overflow-hidden shadow-lg">
+        <div className="bg-gradient-to-r from-teal-400 to-cyan-500 p-4 text-white">
+          <h2 className="text-xl font-bold">Gradient Card 4</h2>
+          <p>Here is some content for the fourth gradient card.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GradientCard;
