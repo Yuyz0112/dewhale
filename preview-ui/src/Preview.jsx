64cf13c5e45932dee381a75e09c066204bd32a84
yz-yu <aryu0112@gmail.com>
2023-12-22 13:47:26 +0800

    [vx.dev] prompt: {   "prompt": "an AST explorer can choose different parsers, the left part is the source code, and the right part is the AST tree viz\r\n\r\n\r\n\nadd a black bg toolbar\ndisplay AST tree with an example value",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/13651389/c7c4357b-a5c2-4aba-9473-3f470c6df92c"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,57 +1,72 @@
-import { Home, Settings, Upload, Download } from 'lucide-react';
-import { Button } from '@/components/ui/button';
+import { Home } from 'lucide-react';
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
 import { Textarea } from '@/components/ui/textarea';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { useState } from 'react';
 
 export default function ASTExplorer() {
-  const [code, setCode] = useState('// Paste or drop some JavaScript here and explore the AST...');
-  const [ast, setAst] = useState(null); // Placeholder for AST data
+  const [code, setCode] = useState(`let tips = [
+  "Click on any AST node with a '+' to expand it",
+  "Hovering over a node highlights the corresponding location in the source code",
+  "Shift click on an AST node to expand the whole subtree"
+];
 
-  // Placeholder function to parse code to AST
-  const parseCodeToAST = (code) => {
-    // This would be replaced with actual parsing logic
-    setAst({ /* AST data structure */ });
+function printTips() {
+  tips.forEach((tip, i) => console.log(\`Tip \${i}: \${tip}\`));
+}`);
+
+  const astExample = {
+    type: 'Program',
+    start: 0,
+    end: 193,
+    body: [
+      {
+        type: 'VariableDeclaration',
+        declarations: [
+          {
+            type: 'VariableDeclarator',
+            id: { type: 'Identifier', name: 'tips' },
+            init: {
+              type: 'ArrayExpression',
+              elements: [
+                { type: 'StringLiteral', value: "Click on any AST node with a '+' to expand it" },
+                { type: 'StringLiteral', value: 'Hovering over a node highlights the corresponding location in the source code' },
+                { type: 'StringLiteral', value: 'Shift click on an AST node to expand the whole subtree' },
+              ],
+            },
+          },
+        ],
+        kind: 'let',
+      },
+      // More AST nodes would go here...
+    ],
   };
 
   return (
-    (<div className="flex flex-col h-screen">
-      <div className="bg-black p-2 flex justify-between items-center">
-        <div className="flex items-center space-x-4">
-          <Button variant="ghost" className="text-white">
-            <Home className="w-5 h-5" />
-          </Button>
-          <Button variant="ghost" className="text-white">
-            <Settings className="w-5 h-5" />
-          </Button>
-        </div>
-        <div className="flex items-center space-x-4">
-          <Button variant="ghost" className="text-white" onClick={() => parseCodeToAST(code)}>
-            <Upload className="w-5 h-5" />
-          </Button>
-          <Button variant="ghost" className="text-white">
-            <Download className="w-5 h-5" />
-          </Button>
-        </div>
-      </div>
-      <div className="flex flex-1">
-        <div className="w-1/2 p-4">
-          <Textarea
-            className="h-full w-full"
-            value={code}
-            onChange={(e) => setCode(e.target.value)}
-          />
-        </div>
-        <div className="w-1/2 p-4 bg-gray-100">
-          {/* Placeholder for AST visualization */}
-          {ast ? (
-            <Home data={ast} />
-          ) : (
-            <div className="flex items-center justify-center h-full">
-              <p className="text-gray-500">AST will be displayed here</p>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>)
+    <div className="flex flex-col h-screen">
+      <header className="bg-black p-4">
+        <h1 className="text-white">AST Explorer</h1>
+      </header>
+      <main className="flex-1 flex overflow-hidden">
+        <Tabs defaultValue="code" className="flex-1 flex">
+          <TabsList className="bg-gray-800">
+            <TabsTrigger value="code" className="text-white">Source Code</TabsTrigger>
+            <TabsTrigger value="ast" className="text-white">AST Tree</TabsTrigger>
+          </TabsList>
+          <TabsContent value="code" className="flex-1 p-4">
+            <Textarea
+              value={code}
+              onChange={(e) => setCode(e.target.value)}
+              className="w-full h-full bg-gray-900 text-white p-4"
+            />
+          </TabsContent>
+          <TabsContent value="ast" className="flex-1 p-4">
+            <ScrollArea className="h-full w-full bg-gray-900 text-white p-4">
+              <pre>{JSON.stringify(astExample, null, 2)}</pre>
+            </ScrollArea>
+          </TabsContent>
+        </Tabs>
+      </main>
+    </div>
   );
 }
