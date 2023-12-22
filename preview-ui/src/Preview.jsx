802e64c11be2d4ef15b2070fb6effe7645da8a32
yz-yu <aryu0112@gmail.com>
2023-12-22 12:47:00 +0800

    [vx.dev] prompt: {   "prompt": "an AST explorer can choose different parsers, the left part is the source code, and the right part is the AST tree viz\r\n\r\n\r\n\nadd toolbar",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/13651389/c7c4357b-a5c2-4aba-9473-3f470c6df92c"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,56 +1,50 @@
+import { Home } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
-import { ScrollArea } from '@/components/ui/scroll-area';
-import { Collapsible, CollapsibleTrigger, CollapsibleContent } from '@/components/ui/collapsible';
-import React from 'react';
+import { Separator } from '@/components/ui/separator';
+import { useState } from 'react';
 
 export default function ASTExplorer() {
+  const [parser, setParser] = useState('babel');
+
   return (
-    <div className="flex h-screen">
-      <div className="w-1/2 p-4">
-        <Textarea className="h-full w-full p-2 border rounded" defaultValue={`/**
-* Paste or drop some JavaScript here and explore
-* the syntax tree created by chosen parser.
-* You can use all the cool new features from ES6
-* and even more. Enjoy!
-*/
-
-let tips = [
-  "Click on any AST node with a '+' to expand it",
-  "Hovering over a node highlights the corresponding location in the source code",
-  "Shift click on an AST node to expand the whole subtree"
-];
-
-function printTips() {
-  tips.forEach((tip, i) => console.log(\`Tip \${i}: \${tip}\`));
-}`} />
-      </div>
-      <div className="w-1/2 p-4">
-        <ScrollArea className="h-full w-full border rounded">
-          <Collapsible>
-            <CollapsibleTrigger className="p-2 cursor-pointer hover:bg-gray-100">ArrayExpression</CollapsibleTrigger>
-            <CollapsibleContent className="pl-4">
-              <Collapsible>
-                <CollapsibleTrigger className="p-2 cursor-pointer hover:bg-gray-100">StringLiteral</CollapsibleTrigger>
-                <CollapsibleContent className="pl-4">
-                  <p className="p-2">"Click on any AST node with a '+' to expand it"</p>
-                </CollapsibleContent>
-              </Collapsible>
-              <Collapsible>
-                <CollapsibleTrigger className="p-2 cursor-pointer hover:bg-gray-100">StringLiteral</CollapsibleTrigger>
-                <CollapsibleContent className="pl-4">
-                  <p className="p-2">"Hovering over a node highlights the corresponding location in the source code"</p>
-                </CollapsibleContent>
-              </Collapsible>
-              <Collapsible>
-                <CollapsibleTrigger className="p-2 cursor-pointer hover:bg-gray-100">StringLiteral</CollapsibleTrigger>
-                <CollapsibleContent className="pl-4">
-                  <p className="p-2">"Shift click on an AST node to expand the whole subtree"</p>
-                </CollapsibleContent>
-              </Collapsible>
-            </CollapsibleContent>
-          </Collapsible>
-        </ScrollArea>
-      </div>
-    </div>
+    (<div className="flex flex-col h-screen">
+      <header className="bg-gray-100 p-4 flex justify-between items-center">
+        <div className="flex space-x-2">
+          <Button variant="outline">Snippet</Button>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue>{parser}</SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="babel" onSelect={() => setParser('babel')}>Babel</SelectItem>
+              <SelectItem value="typescript" onSelect={() => setParser('typescript')}>TypeScript</SelectItem>
+              <SelectItem value="flow" onSelect={() => setParser('flow')}>Flow</SelectItem>
+              <SelectItem value="espree" onSelect={() => setParser('espree')}>Espree</SelectItem>
+            </SelectContent>
+          </Select>
+          <Button variant="outline">Transform</Button>
+        </div>
+        <div className="flex space-x-2">
+          <Button variant="outline">Tree</Button>
+          <Button variant="outline">JSON</Button>
+        </div>
+      </header>
+      <main className="flex-1 flex overflow-hidden">
+        <div className="flex-1 p-4">
+          <Textarea placeholder="Paste or drop some JavaScript here and explore the syntax tree created by chosen parser." className="h-full w-full" />
+        </div>
+        <Separator orientation="vertical" className="bg-gray-300" />
+        <div className="flex-1 p-4">
+          <Home ratio={1}>
+            <div className="bg-gray-200 rounded-md flex justify-center items-center">
+              {/* Placeholder for AST visualization */}
+              <span>AST Tree Visualization</span>
+            </div>
+          </AspectRatio>
+        </div>
+      </main>
+    </div>)
   );
 }
