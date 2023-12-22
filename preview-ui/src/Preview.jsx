7704eb976c377a3a4b5c30fadc696aa1a67bb0c0
yz-yu <aryu0112@gmail.com>
2023-12-22 12:05:13 +0800

    [vx.dev] prompt: {   "prompt": "Build a Web-based file manager \r\n\r\n Clean, modern user interface using UI with light and dark modes.\r\n Optimistic, instant UI updates + loading states with  query.\r\n Drag n‘ drop local + remote uploads.\r\nSupport for individual files and folders\r\nSupports multiple file formats and displays icons according to different types.\r\nSupport for compressing, decompressing, moving and copying files\r\n\r\n\r\n\r\n\n存在运行时错误，重新调整保证正确性\nTableHeader 的类名不正确，请调整为与 TableCell 可以对应。\ntry again",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/18740395/85f7e24e-3ebe-4e2b-bde8-e4f1c222f7af",     "https://github.com/Yuyz0112/vx.dev/assets/18740395/d16c3e56-d247-403f-ae6b-1d5c2c01332a"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,140 +1,105 @@
-import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip';
+import { Folder, Home, Trash, User, Settings, Upload, File, ArrowRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { Upload, Plus, File, Settings, Home, Trash, User, Table, Folder, Image, ArrowRight } from 'lucide-react';
+import { Table, TableCaption, TableHeader, TableRow, TableCell, TableBody } from '@/components/ui/table';
+import { Checkbox } from '@/components/ui/checkbox';
 import { Switch } from '@/components/ui/switch';
-import { TableCaption, TableHead, TableRow, TableCell, TableBody } from '@/components/ui/table';
-import { ContextMenu, ContextMenuTrigger, ContextMenuContent, ContextMenuItem } from '@/components/ui/context-menu';
+import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 import React from 'react';
 
-const FileManager = () => {
+export default function FileManager() {
   return (
-    <div className="flex flex-col h-screen bg-gray-100 dark:bg-gray-800">
-      <header className="flex justify-between items-center p-4 bg-white dark:bg-gray-900 shadow-md">
-        <h1 className="text-xl font-semibold text-gray-900 dark:text-white">Turbo Folder</h1>
-        <div className="flex items-center">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button variant="ghost" className="mr-2">
-                  <Upload className="w-5 h-5 text-gray-600 dark:text-gray-300" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>Upload</TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button variant="ghost" className="mr-2">
-                  <Plus className="w-5 h-5 text-gray-600 dark:text-gray-300" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>New folder</TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button variant="ghost" className="mr-2">
-                  <File className="w-5 h-5 text-gray-600 dark:text-gray-300" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>New file</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-          <Switch className="mr-2" />
-          <Button variant="ghost">
-            <Settings className="w-5 h-5 text-gray-600 dark:text-gray-300" />
+    <div className="flex h-screen bg-gray-100 dark:bg-gray-800">
+      <div className="bg-white dark:bg-gray-900 w-64 p-6 hidden sm:block">
+        <div className="flex items-center space-x-2 mb-5">
+          <Folder className="w-6 h-6" />
+          <span className="font-bold text-lg">Turbo Folder</span>
+        </div>
+        <div className="space-y-2">
+          <Button variant="ghost" className="justify-start w-full">
+            <Home className="w-5 h-5 mr-2" /> My Drive
+          </Button>
+          <Button variant="ghost" className="justify-start w-full">
+            <Trash className="w-5 h-5 mr-2" /> Trash
+          </Button>
+          <Button variant="ghost" className="justify-start w-full">
+            <User className="w-5 h-5 mr-2" /> Users
+          </Button>
+          <Button variant="ghost" className="justify-start w-full">
+            <Settings className="w-5 h-5 mr-2" /> Logs
           </Button>
         </div>
-      </header>
-      <div className="flex flex-1 overflow-hidden">
-        <div className="bg-white dark:bg-gray-900 w-64 p-6 hidden sm:block">
-          <nav className="space-y-2">
-            <Button asChild variant="ghost" className="flex items-center space-x-2">
-              <Home className="w-5 h-5 text-gray-600 dark:text-gray-300" />
-              <span className="text-gray-900 dark:text-white">My Drive</span>
-            </Button>
-            <Button asChild variant="ghost" className="flex items-center space-x-2">
-              <Trash className="w-5 h-5 text-gray-600 dark:text-gray-300" />
-              <span className="text-gray-900 dark:text-white">Trash</span>
-            </Button>
-            <Button asChild variant="ghost" className="flex items-center space-x-2">
-              <User className="w-5 h-5 text-gray-600 dark:text-gray-300" />
-              <span className="text-gray-900 dark:text-white">Users</span>
-            </Button>
-            <Button asChild variant="ghost" className="flex items-center space-x-2">
-              <File className="w-5 h-5 text-gray-600 dark:text-gray-300" />
-              <span className="text-gray-900 dark:text-white">Logs</span>
-            </Button>
-          </nav>
-        </div>
-        <main className="flex-1 overflow-y-auto p-4">
-          <Table>
-            <TableCaption className="text-gray-600 dark:text-gray-300">Total 3 items in the current directory.</TableCaption>
-            <TableHead>
-              <TableRow>
-                <TableCell className="w-[100px]">Name</TableCell>
-                <TableCell>Size</TableCell>
-                <TableCell>Last modified</TableCell>
-                <TableCell>Created at</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell className="font-medium flex items-center">
-                  <Folder className="w-5 h-5 text-yellow-500 mr-2" />
-                  Directory
-                </TableCell>
-                <TableCell>64 B</TableCell>
-                <TableCell>Nov 22, 2023</TableCell>
-                <TableCell>Nov 22, 2023</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="font-medium flex items-center">
-                  <File className="w-5 h-5 text-blue-500 mr-2" />
-                  Document.pdf
-                </TableCell>
-                <TableCell>1.2 MiB</TableCell>
-                <TableCell>Nov 21, 2023</TableCell>
-                <TableCell>Nov 20, 2023</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell className="font-medium flex items-center">
-                  <Image className="w-5 h-5 text-green-500 mr-2" />
-                  Image.png
-                </TableCell>
-                <TableCell>2.5 MiB</TableCell>
-                <TableCell>Nov 20, 2023</TableCell>
-                <TableCell>Nov 19, 2023</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-          <ContextMenu>
-            <ContextMenuTrigger asChild>
-              <Button variant="ghost" className="absolute top-0 right-0 m-4">
-                <ArrowRight className="w-5 h-5 text-gray-600 dark:text-gray-300" />
-              </Button>
-            </ContextMenuTrigger>
-            <ContextMenuContent className="bg-white dark:bg-gray-700 shadow-lg rounded-md">
-              <ContextMenuItem>Open</ContextMenuItem>
-              <ContextMenuItem>Download</ContextMenuItem>
-              <ContextMenuItem>Share</ContextMenuItem>
-              <ContextMenuItem>Move</ContextMenuItem>
-              <ContextMenuItem>Copy</ContextMenuItem>
-              <ContextMenuItem>Compress</ContextMenuItem>
-              <ContextMenuItem>Extract</ContextMenuItem>
-              <ContextMenuItem>Delete</ContextMenuItem>
-            </ContextMenuContent>
-          </ContextMenu>
-        </main>
       </div>
-      <footer className="flex justify-between items-center p-4 bg-white dark:bg-gray-900 shadow-inner">
-        <Button asChild variant="ghost">
-          <span className="text-gray-600 dark:text-gray-300">Theme</span>
-        </Button>
-        <Button asChild variant="ghost">
-          <span className="text-gray-600 dark:text-gray-300">Sign out</span>
-        </Button>
-      </footer>
+      <div className="flex-1 p-6">
+        <Table>
+          <TableCaption className="text-left">My Drive</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableCell><Checkbox /></TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Size</TableCell>
+              <TableCell>Last modified</TableCell>
+              <TableCell>Created at</TableCell>
+              <TableCell>
+                <Button variant="outline" className="mr-2">
+                  <Upload className="w-4 h-4 mr-1" /> Upload
+                </Button>
+                <Button variant="outline" className="mr-2">
+                  <Folder className="w-4 h-4 mr-1" /> New folder
+                </Button>
+                <Button variant="outline">
+                  <File className="w-4 h-4 mr-1" /> New file
+                </Button>
+              </TableCell>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {/* Example file row */}
+            <TableRow>
+              <TableCell><Checkbox /></TableCell>
+              <TableCell>
+                <File className="w-4 h-4 mr-2" />
+                example_document.pdf
+              </TableCell>
+              <TableCell>1.2 MB</TableCell>
+              <TableCell>Jan 1, 2023</TableCell>
+              <TableCell>Dec 1, 2022</TableCell>
+            </TableRow>
+            {/* Example folder row */}
+            <TableRow>
+              <TableCell><Checkbox /></TableCell>
+              <TableCell>
+                <Folder className="w-4 h-4 mr-2" />
+                Photos
+              </TableCell>
+              <TableCell>—</TableCell>
+              <TableCell>Jan 2, 2023</TableCell>
+              <TableCell>Dec 2, 2022</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+      <div className="bg-white dark:bg-gray-900 w-64 p-6 hidden sm:block">
+        <div className="flex justify-between items-center mb-5">
+          <Switch />
+          <Button variant="ghost" className="justify-start">
+            <ArrowRight className="w-5 h-5 mr-2" /> Sign out
+          </Button>
+        </div>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Avatar>
+                <AvatarImage src="https://via.placeholder.com/150" />
+                <AvatarFallback>U</AvatarFallback>
+              </Avatar>
+            </TooltipTrigger>
+            <TooltipContent>
+              User Profile
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
     </div>
   );
-};
-
-export default FileManager;
+}
