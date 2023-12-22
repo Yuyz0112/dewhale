24747e1a54b22fc9e13d9a5ac920cef5b44341d8
yz-yu <aryu0112@gmail.com>
2023-12-22 11:54:44 +0800

    [vx.dev] prompt: {   "prompt": "Build a Web-based file manager \r\n\r\n Clean, modern user interface using UI with light and dark modes.\r\n Optimistic, instant UI updates + loading states with  query.\r\n Drag n‘ drop local + remote uploads.\r\nSupport for individual files and folders\r\nSupports multiple file formats and displays icons according to different types.\r\nSupport for compressing, decompressing, moving and copying files\r\n\r\n\r\n\r\n\n存在运行时错误，重新调整保证正确性\nTableHeader 的类名不正确，请调整为与 TableCell 可以对应。",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/18740395/85f7e24e-3ebe-4e2b-bde8-e4f1c222f7af",     "https://github.com/Yuyz0112/vx.dev/assets/18740395/d16c3e56-d247-403f-ae6b-1d5c2c01332a"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,104 +1,140 @@
-import { Home, Folder, Image, Video, Music, Archive, Upload, Plus, Minus, Trash, User, File, Table } from lucide-react;
-import { Button } from @/components/ui/button;
-import { TableCaption, TableHeader, TableRow, TableHead, TableBody, TableCell } from @/components/ui/table;
-import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from @/components/ui/tooltip;
-import { Switch } from @/components/ui/switch;
+import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip';
+import { Button } from '@/components/ui/button';
+import { Upload, Plus, File, Settings, Home, Trash, User, Table, Folder, Image, ArrowRight } from 'lucide-react';
+import { Switch } from '@/components/ui/switch';
+import { TableCaption, TableHead, TableRow, TableCell, TableBody } from '@/components/ui/table';
+import { ContextMenu, ContextMenuTrigger, ContextMenuContent, ContextMenuItem } from '@/components/ui/context-menu';
 import React from 'react';
 
-export default function FileManager() {
-  // This is a static prototype, so the theme state is not functional.
-  const [darkMode, setDarkMode] = React.useState(false);
-
-  // File icons based on file types
-  const fileIcons = {
-    folder: <Folder />,
-    image: <Image />,
-    video: <Video />,
-    audio: <Music />,
-    document: <Home />,
-    archive: <Archive />,
-  };
-
-  // Static file data
-  const files = [
-    { name: 'Photos', type: 'folder', size: '2 GB', modified: '2023-04-01', created: '2023-01-15' },
-    { name: 'Vacation.mp4', type: 'video', size: '1.5 GB', modified: '2023-03-21', created: '2023-01-10' },
-    { name: 'Favorite Music', type: 'audio', size: '600 MB', modified: '2023-03-15', created: '2023-01-05' },
-    { name: 'Project.docx', type: 'document', size: '4 MB', modified: '2023-03-10', created: '2023-01-01' },
-    { name: 'Archive.zip', type: 'archive', size: '500 MB', modified: '2023-03-05', created: '2022-12-20' },
-  ];
-
+const FileManager = () => {
   return (
-    <div className="flex flex-col h-screen">
-      <header className="bg-white p-4 shadow-md flex justify-between items-center">
-        <h1 className="text-xl font-semibold">Turbo Folder</h1>
-        <div>
-          <Button asChild>
-            <Upload className="w-5 h-5 mr-2" />
-          </Button>
-          <Button asChild>
-            <Plus className="w-5 h-5 mr-2" />
-          </Button>
-          <Button asChild>
-            <Minus className="w-5 h-5" />
+    <div className="flex flex-col h-screen bg-gray-100 dark:bg-gray-800">
+      <header className="flex justify-between items-center p-4 bg-white dark:bg-gray-900 shadow-md">
+        <h1 className="text-xl font-semibold text-gray-900 dark:text-white">Turbo Folder</h1>
+        <div className="flex items-center">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" className="mr-2">
+                  <Upload className="w-5 h-5 text-gray-600 dark:text-gray-300" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>Upload</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" className="mr-2">
+                  <Plus className="w-5 h-5 text-gray-600 dark:text-gray-300" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>New folder</TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" className="mr-2">
+                  <File className="w-5 h-5 text-gray-600 dark:text-gray-300" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>New file</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <Switch className="mr-2" />
+          <Button variant="ghost">
+            <Settings className="w-5 h-5 text-gray-600 dark:text-gray-300" />
           </Button>
         </div>
       </header>
       <div className="flex flex-1 overflow-hidden">
-        <div className="bg-gray-100 w-64 p-4 overflow-y-auto">
-          <Button asChild>
-            <Folder className="w-5 h-5 mr-2" /> My Drive
-          </Button>
-          <Button asChild>
-            <Trash className="w-5 h-5 mr-2" /> Trash
-          </Button>
-          <Button asChild>
-            <User className="w-5 h-5 mr-2" /> Users
-          </Button>
-          <Button asChild>
-            <File className="w-5 h-5 mr-2" /> Logs
-          </Button>
+        <div className="bg-white dark:bg-gray-900 w-64 p-6 hidden sm:block">
+          <nav className="space-y-2">
+            <Button asChild variant="ghost" className="flex items-center space-x-2">
+              <Home className="w-5 h-5 text-gray-600 dark:text-gray-300" />
+              <span className="text-gray-900 dark:text-white">My Drive</span>
+            </Button>
+            <Button asChild variant="ghost" className="flex items-center space-x-2">
+              <Trash className="w-5 h-5 text-gray-600 dark:text-gray-300" />
+              <span className="text-gray-900 dark:text-white">Trash</span>
+            </Button>
+            <Button asChild variant="ghost" className="flex items-center space-x-2">
+              <User className="w-5 h-5 text-gray-600 dark:text-gray-300" />
+              <span className="text-gray-900 dark:text-white">Users</span>
+            </Button>
+            <Button asChild variant="ghost" className="flex items-center space-x-2">
+              <File className="w-5 h-5 text-gray-600 dark:text-gray-300" />
+              <span className="text-gray-900 dark:text-white">Logs</span>
+            </Button>
+          </nav>
         </div>
         <main className="flex-1 overflow-y-auto p-4">
           <Table>
-            <TableCaption>A list of your files and folders.</TableCaption>
-            <TableHeader>
+            <TableCaption className="text-gray-600 dark:text-gray-300">Total 3 items in the current directory.</TableCaption>
+            <TableHead>
               <TableRow>
-                <TableHead>Name</TableHead>
-                <TableHead>Size</TableHead>
-                <TableHead>Last modified</TableHead>
-                <TableHead>Created at</TableHead>
+                <TableCell className="w-[100px]">Name</TableCell>
+                <TableCell>Size</TableCell>
+                <TableCell>Last modified</TableCell>
+                <TableCell>Created at</TableCell>
               </TableRow>
-            </TableHeader>
+            </TableHead>
             <TableBody>
-              {files.map((file, index) => (
-                <TableRow key={index}>
-                  <TableCell>
-                    {fileIcons[file.type]}
-                    {file.name}
-                  </TableCell>
-                  <TableCell>{file.size}</TableCell>
-                  <TableCell>{file.modified}</TableCell>
-                  <TableCell>{file.created}</TableCell>
-                </TableRow>
-              ))}
+              <TableRow>
+                <TableCell className="font-medium flex items-center">
+                  <Folder className="w-5 h-5 text-yellow-500 mr-2" />
+                  Directory
+                </TableCell>
+                <TableCell>64 B</TableCell>
+                <TableCell>Nov 22, 2023</TableCell>
+                <TableCell>Nov 22, 2023</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium flex items-center">
+                  <File className="w-5 h-5 text-blue-500 mr-2" />
+                  Document.pdf
+                </TableCell>
+                <TableCell>1.2 MiB</TableCell>
+                <TableCell>Nov 21, 2023</TableCell>
+                <TableCell>Nov 20, 2023</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium flex items-center">
+                  <Image className="w-5 h-5 text-green-500 mr-2" />
+                  Image.png
+                </TableCell>
+                <TableCell>2.5 MiB</TableCell>
+                <TableCell>Nov 20, 2023</TableCell>
+                <TableCell>Nov 19, 2023</TableCell>
+              </TableRow>
             </TableBody>
           </Table>
+          <ContextMenu>
+            <ContextMenuTrigger asChild>
+              <Button variant="ghost" className="absolute top-0 right-0 m-4">
+                <ArrowRight className="w-5 h-5 text-gray-600 dark:text-gray-300" />
+              </Button>
+            </ContextMenuTrigger>
+            <ContextMenuContent className="bg-white dark:bg-gray-700 shadow-lg rounded-md">
+              <ContextMenuItem>Open</ContextMenuItem>
+              <ContextMenuItem>Download</ContextMenuItem>
+              <ContextMenuItem>Share</ContextMenuItem>
+              <ContextMenuItem>Move</ContextMenuItem>
+              <ContextMenuItem>Copy</ContextMenuItem>
+              <ContextMenuItem>Compress</ContextMenuItem>
+              <ContextMenuItem>Extract</ContextMenuItem>
+              <ContextMenuItem>Delete</ContextMenuItem>
+            </ContextMenuContent>
+          </ContextMenu>
         </main>
       </div>
-      <footer className="bg-white p-4 shadow-md flex justify-between items-center">
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
-            </TooltipTrigger>
-            <TooltipContent>Toggle Theme</TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <Button asChild>
-          <Minus className="w-5 h-5" /> Sign out
+      <footer className="flex justify-between items-center p-4 bg-white dark:bg-gray-900 shadow-inner">
+        <Button asChild variant="ghost">
+          <span className="text-gray-600 dark:text-gray-300">Theme</span>
+        </Button>
+        <Button asChild variant="ghost">
+          <span className="text-gray-600 dark:text-gray-300">Sign out</span>
         </Button>
       </footer>
     </div>
   );
-}
+};
+
+export default FileManager;
