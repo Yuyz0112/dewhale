b6b4369605f993b241f30df1d4ddda4a614e601e
yz-yu <aryu0112@gmail.com>
2023-12-22 18:00:59 +0800

    [vx.dev] prompt: {   "prompt": "product page for a health app, looks fresh\r\n\r\n\r\n\nadd some green texts",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/13651389/b38dcedd-5ea0-4af8-884d-52768f75ca1c"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,14 +1,14 @@
 import { Button } from '@/components/ui/button';
-import { Download, Star, ArrowRight } from 'lucide-react';
-import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 import { Badge } from '@/components/ui/badge';
-import { AspectRatio } from '@/components/ui/aspect-ratio';
+import { Download, ArrowRight, Frown, Meh, Smile, Star, Home } from 'lucide-react';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 import React from 'react';
 
-export default function HealthAppProductPage() {
+const ProductPage = () => {
   return (
-    <div className="bg-white text-gray-700">
-      <header className="flex justify-between items-center p-4 border-b">
+    <div className="bg-white text-gray-800">
+      <header className="bg-white p-4 shadow-md flex justify-between items-center">
         <nav className="flex space-x-4">
           <Button variant="ghost">Services</Button>
           <Button variant="ghost">How it works</Button>
@@ -16,86 +16,79 @@ export default function HealthAppProductPage() {
         </nav>
         <Button variant="outline">Log in</Button>
       </header>
-      <main className="p-8">
-        <section className="text-center">
-          <h1 className="text-6xl font-bold mb-4">HEALTH INFO IN ONE APP</h1>
-          <p className="text-lg mb-6">Doctors, medicine, lab tests – all in one service 24/7 availability...</p>
+      <main className="flex-1 overflow-y-auto p-4">
+        <section className="text-center my-12">
+          <h1 className="text-6xl font-bold text-gray-800 mb-4">HEALTH INFO <Badge variant="outline" className="text-green-500">IN ONE APP</Badge></h1>
+          <p className="text-lg text-gray-600 mb-6">Doctors, medicine, lab tests – all in one service 24/7 availability...</p>
           <div className="flex justify-center space-x-2 mb-8">
-            <Button variant="solid">
+            <Button variant="solid" className="bg-green-500 text-white">
               <Download className="w-4 h-4 mr-2" /> Download on the App Store
             </Button>
-            <Button variant="solid">
+            <Button variant="solid" className="bg-green-500 text-white">
               <Download className="w-4 h-4 mr-2" /> Get it on Google Play
             </Button>
           </div>
+          <a href="#details" className="text-green-500 hover:underline">See details <ArrowRight className="inline-block" /></a>
         </section>
-        <section className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-          <div className="space-y-4">
-            <div className="bg-orange-100 p-4 rounded-lg shadow-md">
-              <p className="mb-2">There is a little fatigue, but in general I feel good</p>
-              <ToggleGroup type="single" defaultValue="neutral">
-                <ToggleGroupItem value="sad" aria-label="Sad">
-                  😔
-                </ToggleGroupItem>
-                <ToggleGroupItem value="neutral" aria-label="Neutral">
-                  🙂
-                </ToggleGroupItem>
-                <ToggleGroupItem value="happy" aria-label="Happy">
-                  😄
-                </ToggleGroupItem>
-              </ToggleGroup>
-            </div>
-            <div className="bg-purple-100 p-4 rounded-lg shadow-md">
-              <h3 className="font-semibold mb-4">What symptoms troubling you today?</h3>
-              <div className="flex justify-around">
-                <Badge variant="outline">Chest pain</Badge>
-                <Badge variant="outline">Headache</Badge>
-                <Badge variant="outline">Fever</Badge>
-                <Badge variant="outline">Cough</Badge>
+        <section className="grid grid-cols-1 md:grid-cols-2 gap-8 my-12">
+          <Card className="bg-green-50">
+            <CardHeader>
+              <CardTitle>What symptoms are troubling you today?</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-wrap justify-center gap-2">
+                <Badge variant="outline" className="text-green-500">Chest pain</Badge>
+                <Badge variant="outline" className="text-green-500">Headache</Badge>
+                <Badge variant="outline" className="text-green-500">Fever</Badge>
+                <Badge variant="outline" className="text-green-500">Cough</Badge>
               </div>
-            </div>
-            <div className="bg-purple-100 p-4 rounded-lg shadow-md">
-              <h3 className="font-semibold mb-4">What level of tiredness do you have now?</h3>
-              <AspectRatio ratio={16 / 9}>
-                <img
-                  src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-                  alt="Tiredness Level"
-                  className="rounded-md object-cover"
-                />
-              </AspectRatio>
-            </div>
-          </div>
-          <div className="space-y-6">
-            <div className="flex items-center justify-center">
-              <AspectRatio ratio={1} className="w-32 h-32">
-                <img
-                  src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-                  alt="Doctor"
-                  className="rounded-full object-cover"
-                />
-              </AspectRatio>
-              <div className="ml-4">
-                <h3 className="text-lg font-semibold">+100k happy clients</h3>
-                <div className="flex items-center mt-2">
-                  <Star className="w-5 h-5 text-yellow-400" />
-                  <Star className="w-5 h-5 text-yellow-400" />
-                  <Star className="w-5 h-5 text-yellow-400" />
-                  <Star className="w-5 h-5 text-yellow-400" />
-                  <Star className="w-5 h-5 text-yellow-400" />
-                </div>
-                <p className="text-sm">App store rating</p>
-                <p className="font-bold">4.9</p>
-                <p className="text-sm">456 reviews</p>
+            </CardContent>
+          </Card>
+          <Card className="bg-green-50">
+            <CardHeader>
+              <CardTitle>What level of tiredness do you have now?</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="flex justify-center items-center space-x-2">
+                <Frown className="text-yellow-400 w-6 h-6" />
+                <Meh className="text-yellow-500 w-6 h-6" />
+                <Smile className="text-green-500 w-6 h-6" />
               </div>
+            </CardContent>
+          </Card>
+        </section>
+        <section className="my-12">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div className="flex justify-center">
+              <Avatar>
+                <AvatarImage src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80" alt="Doctor" />
+                <AvatarFallback>Dr</AvatarFallback>
+              </Avatar>
             </div>
-            <div className="text-right">
-              <Button variant="link">
-                See details <ArrowRight className="w-4 h-4 ml-2" />
-              </Button>
+            <div className="text-center">
+              <h2 className="text-3xl font-bold text-gray-800 mb-4">+100k happy clients</h2>
+              <p className="text-lg text-gray-600">Join our community and start improving your health today.</p>
+            </div>
+            <div className="text-center">
+              <h2 className="text-3xl font-bold text-gray-800 mb-4">App store rating</h2>
+              <div className="flex justify-center items-center">
+                <Star className="text-yellow-400 w-6 h-6" />
+                <Star className="text-yellow-400 w-6 h-6" />
+                <Star className="text-yellow-400 w-6 h-6" />
+                <Star className="text-yellow-400 w-6 h-6" />
+                <Star className="text-yellow-400 w-6 h-6" />
+                <span className="text-lg text-gray-600 ml-2">4.9</span>
+              </div>
+              <p className="text-sm text-gray-500">456 reviews</p>
             </div>
           </div>
         </section>
       </main>
+      <footer className="bg-white p-4 shadow-md">
+        <p className="text-center text-sm text-gray-600">© 2023 Health App, Inc. All rights reserved.</p>
+      </footer>
     </div>
   );
-}
+};
+
+export default ProductPage;
