858b660ae9dbc4591589805e296baad9d65964e1
yz-yu <aryu0112@gmail.com>
2023-12-23 00:30:44 +0800

    [vx.dev] prompt: {   "prompt": "create a mobile app, looks like discord\nuse purple as the primary color\n\nadd some chat\n\nadd more channels",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,78 +1,107 @@
+import { Home, Plus, Settings, MessageSquare, Users, User, Send } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { Search, MessageSquare, Users, Settings, Home, User } from 'lucide-react';
-import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Input } from '@/components/ui/input';
-import React from 'react';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { useState } from 'react';
 
-export default function DiscordApp() {
+export default function MobileApp() {
+  const [currentTab, setCurrentTab] = useState('chat');
+
   return (
-    <div className="flex flex-col h-screen bg-gray-800 text-white">
-      <header className="flex items-center justify-between p-4 bg-gray-900 shadow-md">
-        <h1 className="text-xl font-bold">Discord</h1>
-        <div className="flex items-center space-x-3">
+    (<div className="flex flex-col h-screen bg-purple-700 text-white">
+      <header className="flex items-center justify-between p-4 bg-purple-800">
+        <h1 className="text-xl font-bold">Discord Clone</h1>
+        <div className="flex space-x-2">
           <Button variant="ghost">
-            <Search className="w-6 h-6" />
+            <Plus className="w-6 h-6" />
           </Button>
-          <Avatar>
-            <AvatarImage src="https://github.com/Yuyz0112.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+          <Button variant="ghost">
+            <Settings className="w-6 h-6" />
+          </Button>
         </div>
       </header>
       <main className="flex-1 overflow-hidden">
-        <Tabs defaultValue="chats" className="flex h-full">
-          <div className="bg-gray-700 w-16 flex flex-col items-center justify-between p-2">
-            <TabsList className="flex flex-col space-y-2">
-              <TabsTrigger value="chats" className="p-2 rounded hover:bg-gray-600">
-                <MessageSquare className="w-6 h-6" />
-              </TabsTrigger>
-              <TabsTrigger value="friends" className="p-2 rounded hover:bg-gray-600">
-                <Users className="w-6 h-6" />
-              </TabsTrigger>
-              <TabsTrigger value="settings" className="p-2 rounded hover:bg-gray-600">
-                <Settings className="w-6 h-6" />
-              </TabsTrigger>
-            </TabsList>
-            <Button variant="ghost" className="mb-2">
-              <Home className="w-6 h-6" />
-            </Button>
-          </div>
-          <div className="flex-1">
-            <TabsContent value="chats" className="h-full">
-              <ScrollArea className="h-full p-4 space-y-4">
-                {/* This is where the chat list would go */}
-                <p>Chat 1</p>
-                <p>Chat 2</p>
-                <p>Chat 3</p>
-              </ScrollArea>
-            </TabsContent>
-            <TabsContent value="friends" className="h-full">
-              <ScrollArea className="h-full p-4 space-y-4">
-                {/* This is where the friends list would go */}
-                <p>Friend 1</p>
-                <p>Friend 2</p>
-                <p>Friend 3</p>
-              </ScrollArea>
-            </TabsContent>
-            <TabsContent value="settings" className="h-full">
-              <ScrollArea className="h-full p-4 space-y-4">
-                {/* This is where the settings would go */}
-                <p>Setting 1</p>
-                <p>Setting 2</p>
-                <p>Setting 3</p>
-              </ScrollArea>
-            </TabsContent>
-          </div>
+        <Tabs defaultValue="chat" className="flex h-full">
+          <TabsList className="flex-none w-16 bg-purple-900 flex flex-col items-center p-2 space-y-2">
+            <TabsTrigger value="chat" onClick={() => setCurrentTab('chat')} className={`w-full p-2 rounded ${currentTab === 'chat' ? 'bg-purple-600' : ''}`}>
+              <MessageSquare className="w-6 h-6" />
+            </TabsTrigger>
+            <TabsTrigger value="channels" onClick={() => setCurrentTab('channels')} className={`w-full p-2 rounded ${currentTab === 'channels' ? 'bg-purple-600' : ''}`}>
+              <Users className="w-6 h-6" />
+            </TabsTrigger>
+            <TabsTrigger value="profile" onClick={() => setCurrentTab('profile')} className={`w-full p-2 rounded ${currentTab === 'profile' ? 'bg-purple-600' : ''}`}>
+              <User className="w-6 h-6" />
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="chat" className="flex-1 flex flex-col bg-purple-600">
+            <ScrollArea className="flex-1 p-4 space-y-4">
+              <Home isOwn={true} />
+              <Home />
+              <Home />
+              <Home isOwn={true} />
+              <Home />
+            </ScrollArea>
+            <div className="p-4 flex items-center space-x-2">
+              <Input className="flex-1 rounded p-2" placeholder="Type a message..." />
+              <Button variant="ghost">
+                <Send className="w-6 h-6" />
+              </Button>
+            </div>
+          </TabsContent>
+          <TabsContent value="channels" className="flex-1 bg-purple-600 p-4">
+            <ScrollArea className="space-y-2">
+              <Home name="# general" />
+              <Home name="# random" />
+              <Home name="# help" />
+              <Home name="# announcements" />
+              <Home name="# events" />
+            </ScrollArea>
+          </TabsContent>
+          <TabsContent value="profile" className="flex-1 bg-purple-600 p-4">
+            <div className="flex items-center space-x-4">
+              <Avatar>
+                <AvatarImage src="https://github.com/Yuyz0112.png" />
+                <AvatarFallback>U</AvatarFallback>
+              </Avatar>
+              <div>
+                <h2 className="text-xl font-bold">Username</h2>
+                <p>#1234</p>
+              </div>
+            </div>
+          </TabsContent>
         </Tabs>
       </main>
-      <footer className="flex items-center justify-between p-4 bg-gray-900">
-        <Input placeholder="Type a message..." className="flex-1 text-black rounded px-4" />
-        <Button variant="ghost" className="ml-2">
-          <User className="w-6 h-6" />
+      <footer className="flex justify-around p-2 bg-purple-800">
+        <Button variant="ghost">
+          <Home className="w-6 h-6" />
+        </Button>
+        <Button variant="ghost">
+          <MessageSquare className="w-6 h-6" />
+        </Button>
+        <Button variant="ghost">
+          <Users className="w-6 h-6" />
         </Button>
       </footer>
+    </div>)
+  );
+}
+
+function ChatMessage({ isOwn }) {
+  return (
+    <div className={`flex ${isOwn ? 'justify-end' : ''}`}>
+      <div className={`max-w-xs p-2 rounded-lg ${isOwn ? 'bg-purple-500' : 'bg-purple-400'}`}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+  );
+}
+
+function ChannelItem({ name }) {
+  return (
+    <div className="p-2 rounded hover:bg-purple-500">
+      <p>{name}</p>
     </div>
   );
 }
