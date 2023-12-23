196a4d8a46b59d1ca7813ab1f08494f4f63d0635
yz-yu <aryu0112@gmail.com>
2023-12-23 17:57:03 +0800

    [vx.dev] prompt: {   "prompt": "apple fruit market analysis report\nadd apple juice section\n你之前的代码有一个错误\n\nCannot read properties of undefined (reading 'map\n\n请你修正",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -4,7 +4,7 @@ import { ResponsiveBar } from '@nivo/bar';
 import { ResponsiveLine } from '@nivo/line';
 import { ResponsivePie } from '@nivo/pie';
 import { AspectRatio } from '@/components/ui/aspect-ratio';
-import { Table, TableCaption, TableHeader, TableRow, TableHead, TableBody } from '@/components/ui/table';
+import { Table, TableCaption, TableHeader, TableRow, TableHead, TableBody, TableCell } from '@/components/ui/table';
 import React from 'react';
 
 export default function AppleFruitMarketAnalysisReport() {
@@ -26,21 +26,74 @@ export default function AppleFruitMarketAnalysisReport() {
             <TabsContent value="sales">
               <div className="h-64">
                 <ResponsiveBar
-                  // ... existing bar chart data and properties
+                  data={[
+                    { country: 'USA', sales: 500 },
+                    { country: 'China', sales: 600 },
+                    { country: 'India', sales: 300 },
+                  ]}
+                  keys={['sales']}
+                  indexBy="country"
+                  margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+                  padding={0.3}
+                  valueScale={{ type: 'linear' }}
+                  indexScale={{ type: 'band', round: true }}
+                  colors={{ scheme: 'nivo' }}
+                  borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+                  labelSkipWidth={12}
+                  labelSkipHeight={12}
+                  labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
                 />
               </div>
             </TabsContent>
             <TabsContent value="trends">
               <div className="h-64">
                 <ResponsiveLine
-                  // ... existing line chart data and properties
+                  data={[
+                    {
+                      id: 'Organic',
+                      data: [
+                        { x: '2018', y: 20 },
+                        { x: '2019', y: 50 },
+                        { x: '2020', y: 80 },
+                      ],
+                    },
+                    {
+                      id: 'Non-organic',
+                      data: [
+                        { x: '2018', y: 10 },
+                        { x: '2019', y: 30 },
+                        { x: '2020', y: 60 },
+                      ],
+                    },
+                  ]}
+                  margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                  xScale={{ type: 'point' }}
+                  yScale={{
+                    type: 'linear',
+                    min: 0,
+                    max: 'auto',
+                  }}
+                  colors={{ scheme: 'category10' }}
                 />
               </div>
             </TabsContent>
             <TabsContent value="market-share">
               <div className="h-64">
                 <ResponsivePie
-                  // ... existing pie chart data and properties
+                  data={[
+                    { id: 'USA', value: 40 },
+                    { id: 'China', value: 30 },
+                    { id: 'India', value: 20 },
+                    { id: 'Others', value: 10 },
+                  ]}
+                  margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+                  innerRadius={0.5}
+                  padAngle={0.7}
+                  cornerRadius={3}
+                  activeOuterRadiusOffset={8}
+                  colors={{ scheme: 'set2' }}
+                  borderWidth={1}
+                  borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
                 />
               </div>
             </TabsContent>
@@ -85,7 +138,31 @@ export default function AppleFruitMarketAnalysisReport() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {/* ... existing table rows */}
+                <TableRow>
+                  <TableCell>China</TableCell>
+                  <TableCell>41,000,000</TableCell>
+                  <TableCell>49%</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>United States</TableCell>
+                  <TableCell>4,649,323</TableCell>
+                  <TableCell>6%</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Poland</TableCell>
+                  <TableCell>3,604,271</TableCell>
+                  <TableCell>4%</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>India</TableCell>
+                  <TableCell>2,500,000</TableCell>
+                  <TableCell>3%</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Iran</TableCell>
+                  <TableCell>2,200,000</TableCell>
+                  <TableCell>2.8%</TableCell>
+                </TableRow>
               </TableBody>
             </Table>
           </AspectRatio>
