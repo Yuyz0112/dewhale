d6e130ffbdc53d3a21bc66e6ed6ba265ac150bdd
yz-yu <aryu0112@gmail.com>
2023-12-23 09:16:11 +0800

    [vx.dev] prompt: {   "prompt": "create a k8s cluster dashboard, manage multiple resources, display a deployment list in the table, display a deployment detail in a sidebar\nadd a nav menu to navigate between different k8s resources\r\nalso add a namespace select to the header",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,98 +1,122 @@
-import { Table, TableCaption, TableHeader, TableRow, TableHead, TableBody, TableCell } from '@/components/ui/table';
-import { Badge } from '@/components/ui/badge';
-import { Button } from '@/components/ui/button';
-import { Home, ChevronRight, X, Settings } from 'lucide-react';
-import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink } from '@/components/ui/navigation-menu';
+import { Server, Database, Layers, Settings, Home } from 'lucide-react';
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/components/ui/select';
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from '@/components/ui/table';
+import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetDescription } from '@/components/ui/sheet';
 import { useState } from 'react';
 
-const deployments = [
-  { id: 1, name: 'Frontend', replicas: 3, status: 'Running', image: 'nginx:latest', age: '34m' },
-  { id: 2, name: 'Backend', replicas: 2, status: 'Updating', image: 'node:alpine', age: '1h' },
-  { id: 3, name: 'Database', replicas: 1, status: 'Running', image: 'mysql:5.7', age: '2d' },
-];
-
-export default function KubernetesClusterDashboard() {
+export default function K8sClusterDashboard() {
+  const [selectedNamespace, setSelectedNamespace] = useState('default');
   const [selectedDeployment, setSelectedDeployment] = useState(null);
-  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  const handleDeploymentClick = (deployment) => {
-    setSelectedDeployment(deployment);
-    setSidebarOpen(true);
-  };
+  const deployments = [
+    { name: 'frontend', replicas: 2, available: 2, updated: '3 minutes ago' },
+    { name: 'backend', replicas: 3, available: 3, updated: '10 minutes ago' },
+    { name: 'database', replicas: 1, available: 1, updated: '1 hour ago' },
+  ];
+
+  const namespaces = ['default', 'production', 'development', 'staging'];
 
   return (
     (<div className="flex h-screen bg-gray-100">
-      <div className="flex-1 p-6">
-        <Table>
-          <TableCaption>Deployment List</TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Name</TableHead>
-              <TableHead>Replicas</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Image</TableHead>
-              <TableHead>Age</TableHead>
-              <TableHead>Actions</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {deployments.map((deployment) => (
-              <TableRow key={deployment.id}>
-                <TableCell>{deployment.name}</TableCell>
-                <TableCell>{deployment.replicas}</TableCell>
-                <TableCell>
-                  <Badge variant={deployment.status === 'Running' ? 'solid' : 'outline'}>
-                    {deployment.status}
-                  </Badge>
-                </TableCell>
-                <TableCell>{deployment.image}</TableCell>
-                <TableCell>{deployment.age}</TableCell>
-                <TableCell>
-                  <Button variant="outline" onClick={() => handleDeploymentClick(deployment)}>
-                    Details
-                  </Button>
-                </TableCell>
+      <NavigationMenu>
+        <NavigationMenuList className="bg-white w-64 p-6 hidden sm:block">
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <Server className="inline-block w-5 h-5 mr-2" /> Deployments
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <Database className="inline-block w-5 h-5 mr-2" /> StatefulSets
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <Layers className="inline-block w-5 h-5 mr-2" /> Services
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink>
+              <Settings className="inline-block w-5 h-5 mr-2" /> ConfigMaps
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="flex-1 flex flex-col">
+        <header className="bg-white p-4 shadow-md flex justify-between items-center">
+          <h1 className="text-xl font-semibold">Kubernetes Cluster Dashboard</h1>
+          <Select className="w-40">
+            <SelectTrigger>
+              <SelectValue>{selectedNamespace}</SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              {namespaces.map((namespace) => (
+                <SelectItem
+                  key={namespace}
+                  value={namespace}
+                  onSelect={() => setSelectedNamespace(namespace)}
+                >
+                  {namespace}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </header>
+
+        <main className="flex-1 overflow-y-auto p-4">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Name</TableHead>
+                <TableHead>Replicas</TableHead>
+                <TableHead>Available</TableHead>
+                <TableHead>Last Updated</TableHead>
+                <TableHead>Actions</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {deployments.map((deployment) => (
+                <TableRow key={deployment.name}>
+                  <TableCell>{deployment.name}</TableCell>
+                  <TableCell>{deployment.replicas}</TableCell>
+                  <TableCell>{deployment.available}</TableCell>
+                  <TableCell>{deployment.updated}</TableCell>
+                  <TableCell>
+                    <Sheet>
+                      <SheetTrigger asChild>
+                        <button
+                          className="text-blue-600 hover:underline"
+                          onClick={() => setSelectedDeployment(deployment)}
+                        >
+                          View Details
+                        </button>
+                      </SheetTrigger>
+                    </Sheet>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </main>
       </div>
-      <Home open={sidebarOpen} onOpenChange={setSidebarOpen}>
-        <Home className="hidden">
-          <ChevronRight />
-        </Home>
-        <Home className="w-80 bg-white p-4">
-          <div className="flex justify-between items-center">
-            <h2 className="text-lg font-semibold">Deployment Details</h2>
-            <Home onClick={() => setSidebarOpen(false)}>
-              <X className="w-5 h-5" />
-            </Home>
-          </div>
-          {selectedDeployment && (
+      {selectedDeployment && (
+        <div>
+          <SheetContent className="w-80 bg-white p-6">
+            <SheetHeader>
+              <SheetTitle>{selectedDeployment.name}</SheetTitle>
+              <SheetDescription>
+                Detailed information about the deployment.
+              </SheetDescription>
+            </SheetHeader>
             <div className="mt-4">
-              <div className="flex items-center space-x-3">
-                <Avatar>
-                  <AvatarImage src={`https://unsplash.it/36/36?image=${selectedDeployment.id}`} />
-                  <AvatarFallback>{selectedDeployment.name[0]}</AvatarFallback>
-                </Avatar>
-                <h3 className="text-xl font-bold">{selectedDeployment.name}</h3>
-              </div>
-              <div className="mt-2">
-                <p><strong>Replicas:</strong> {selectedDeployment.replicas}</p>
-                <p><strong>Status:</strong> {selectedDeployment.status}</p>
-                <p><strong>Image:</strong> {selectedDeployment.image}</p>
-                <p><strong>Age:</strong> {selectedDeployment.age}</p>
-              </div>
-              <div className="mt-4">
-                <Button variant="solid" className="w-full">
-                  <Settings className="w-4 h-4 mr-2" />
-                  Manage Deployment
-                </Button>
-              </div>
+              <p><strong>Replicas:</strong> {selectedDeployment.replicas}</p>
+              <p><strong>Available:</strong> {selectedDeployment.available}</p>
+              <p><strong>Last Updated:</strong> {selectedDeployment.updated}</p>
             </div>
-          )}
-        </Home>
-      </Home>
+          </SheetContent>
+        </div>
+      )}
     </div>)
   );
 }
