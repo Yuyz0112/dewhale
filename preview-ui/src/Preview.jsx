b5b9e14b6744c8c91897f384259293b5c2831957
yz-yu <aryu0112@gmail.com>
2023-12-26 23:06:25 +0800

    [vx.dev] prompt: {   "prompt": "\r\n\r\n创建一个移动端聊天 App\n",   "images": [     "https://github.com/Yuyz0112/vx.dev/assets/13651389/e65c2869-be87-4769-ab1e-81583f75931c"   ] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,1 +1,48 @@
-export default function VxDev() { return <p>vx.dev placeholder</p>; }
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { Plus, Send } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+import React from 'react';
+
+export default function ChatApp() {
+  return (
+    <div className="flex flex-col h-screen bg-gray-900 text-white">
+      <header className="flex items-center justify-between p-4 border-b border-gray-700">
+        <div className="flex items-center space-x-3">
+          <Avatar>
+            <AvatarImage src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=60&dpr=2&q=80" />
+            <AvatarFallback>SD</AvatarFallback>
+          </Avatar>
+          <div>
+            <div className="font-semibold">Sofia Davis</div>
+            <div className="text-gray-400 text-sm">m@example.com</div>
+          </div>
+        </div>
+        <button className="p-2 rounded-full bg-gray-700">
+          <Plus className="w-6 h-6" />
+        </button>
+      </header>
+      <div className="flex-1 p-4 space-y-4 overflow-y-auto">
+        <div className="flex flex-col space-y-2">
+          <div className="self-start bg-gray-700 rounded px-4 py-2 max-w-xs">
+            Hi, how can I help you today?
+          </div>
+          <div className="self-end bg-orange-500 rounded px-4 py-2 max-w-xs">
+            Hey, I'm having trouble with my account.
+          </div>
+          <div className="self-start bg-gray-700 rounded px-4 py-2 max-w-xs">
+            What seems to be the problem?
+          </div>
+          <div className="self-end bg-orange-500 rounded px-4 py-2 max-w-xs">
+            I can't log in.
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-between p-4 bg-gray-800">
+        <Input placeholder="Type your message..." className="flex-1 text-black rounded px-4 py-2" />
+        <button className="ml-4 p-2 rounded-full bg-orange-500">
+          <Send className="w-6 h-6" />
+        </button>
+      </div>
+    </div>
+  );
+}
