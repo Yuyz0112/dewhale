4da1267037b6dfbdd0d1caabd43609c8758f9873
yz-yu <aryu0112@gmail.com>
2023-12-28 21:53:48 +0800

    [vx.dev] prompt: {   "prompt": "创建一个前端组件展示网站。\r\n左侧为菜单，有组件、hook、wiki 几大类，大类中可点击切换具体内容。\r\n右侧展示具体信息，若为组件，则展示简介、基本演示与示例代码、各属性对应的演示与示例代码、属性列表、slot 列表、事件列表；若为 hook，则展示简介、基本演示与示例代码、入参列表、返回值列表。若为 wiki 则展示文章内容。\n",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,1 +1,87 @@
-export default function VxDev() { return <p>vx.dev placeholder</p>; }
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from '@/components/ui/accordion';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import React from 'react';
+
+export default function ComponentShowcase() {
+  return (
+    <div className="flex h-screen">
+      <div className="bg-white w-64 p-6 hidden sm:block">
+        <ScrollArea className="h-full">
+          <Tabs defaultValue="components" className="flex flex-col">
+            <TabsList>
+              <TabsTrigger value="components">Components</TabsTrigger>
+              <TabsTrigger value="hooks">Hooks</TabsTrigger>
+              <TabsTrigger value="wiki">Wiki</TabsTrigger>
+            </TabsList>
+            <TabsContent value="components" className="mt-4">
+              <Accordion type="single" collapsible>
+                <AccordionItem value="accordion">
+                  <AccordionTrigger>Accordion</AccordionTrigger>
+                </AccordionItem>
+                <AccordionItem value="button">
+                  <AccordionTrigger>Button</AccordionTrigger>
+                </AccordionItem>
+                <AccordionItem value="card">
+                  <AccordionTrigger>Card</AccordionTrigger>
+                </AccordionItem>
+              </Accordion>
+            </TabsContent>
+            <TabsContent value="hooks" className="mt-4">
+              {/* Hooks list can be added here */}
+            </TabsContent>
+            <TabsContent value="wiki" className="mt-4">
+              {/* Wiki articles list can be added here */}
+            </TabsContent>
+          </Tabs>
+        </ScrollArea>
+      </div>
+      <div className="flex-1 p-6 overflow-y-auto">
+        <Tabs defaultValue="components">
+          <TabsContent value="components">
+            <Card>
+              <CardHeader>
+                <CardTitle>Accordion Component</CardTitle>
+                <CardDescription>A collapsible component to show/hide content</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Basic Demo:</p>
+                <Accordion type="single" collapsible>
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionContent>
+                      Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+                <Separator />
+                <p>Example Code:</p>
+                <ScrollArea className="h-[200px] w-full rounded-md border p-4">
+                  <code>
+                    {`<Accordion type="single" collapsible>
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>`}
+                  </code>
+                </ScrollArea>
+                {/* More demos and code examples can be added here */}
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="hooks">
+            {/* Hook details can be added here */}
+          </TabsContent>
+          <TabsContent value="wiki">
+            {/* Wiki content can be added here */}
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+}
