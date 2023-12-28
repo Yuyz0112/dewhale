08164d646a22a89d97a7d15130da0091ab99a822
yz-yu <aryu0112@gmail.com>
2023-12-28 22:25:29 +0800

    [vx.dev] prompt: {   "prompt": "创建一个节点图可视化页面。\r\n页面布局为，顶部bar包括上传网络信息、测试、用户登录按钮。\r\n中间为根据后端提供的网络节点拓扑图生成可视化图像，图的边记录两端节点的信息。节点点开后进入二级菜单查看节点信息。\r\n图网络先使用随机生成的节点和边填充。\r\n底部为用户信息介绍。\n之前的代码中你尝试使用 ResponsiveNetwork，但是我们目前还不支持。你可以使用 DOM 实现一个 network graph 吗？",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button';
 import { UploadCloud, Zap, User } from 'lucide-react';
-import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card';
 import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card';
 import React from 'react';
 
 const generateData = () => {
@@ -44,18 +44,27 @@ const NetworkVisualizationPage = () => {
       <main className="flex-1 overflow-hidden p-4">
         <div className="w-full h-full bg-white rounded shadow relative">
           {nodes.map(node => (
-            <div
-              key={node.id}
-              className="absolute bg-blue-500 rounded-full text-white flex items-center justify-center"
-              style={{
-                width: '20px',
-                height: '20px',
-                transform: `translate(${Math.random() * 100}%, ${Math.random() * 100}%)`,
-              }}
-              title={node.label}
-            >
-              {node.label}
-            </div>
+            <Dialog key={node.id}>
+              <DialogTrigger asChild>
+                <div
+                  className="absolute bg-blue-500 rounded-full text-white flex items-center justify-center cursor-pointer"
+                  style={{
+                    width: '20px',
+                    height: '20px',
+                    transform: `translate(${Math.random() * 100}%, ${Math.random() * 100}%)`,
+                  }}
+                  title={node.label}
+                >
+                  {node.label}
+                </div>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>{node.label}</DialogTitle>
+                </DialogHeader>
+                <p>Details about {node.label} will be displayed here.</p>
+              </DialogContent>
+            </Dialog>
           ))}
           {links.map((link, index) => (
             <svg key={index} className="absolute w-full h-full top-0 left-0">
@@ -90,17 +99,6 @@ const NetworkVisualizationPage = () => {
           </CardFooter>
         </Card>
       </footer>
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="ghost">View Node Info</Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Node Information</DialogTitle>
-          </DialogHeader>
-          <p>Details about the node will be displayed here.</p>
-        </DialogContent>
-      </Dialog>
     </div>
   );
 };
