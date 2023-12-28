216d229a207a1a8a0a370406517717d7f9d1e7f3
yz-yu <aryu0112@gmail.com>
2023-12-28 22:22:06 +0800

    [vx.dev] prompt: {   "prompt": "创建一个节点图可视化页面。\r\n页面布局为，顶部bar包括上传网络信息、测试、用户登录按钮。\r\n中间为根据后端提供的网络节点拓扑图生成可视化图像，图的边记录两端节点的信息。节点点开后进入二级菜单查看节点信息。\r\n图网络先使用随机生成的节点和边填充。\r\n底部为用户信息介绍。\n之前的代码中你尝试使用 ResponsiveNetwork，但是我们目前还不支持。你可以使用 svg 或者 DOM 实现一个简易的 network graph 吗？",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import { UploadCloud, Zap, User, Home } from 'lucide-react';
+import { UploadCloud, Zap, User } from 'lucide-react';
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card';
 import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import React from 'react';
@@ -7,22 +7,24 @@ import React from 'react';
 const generateData = () => {
   const nodes = Array.from({ length: 10 }, (_, i) => ({
     id: `node-${i}`,
+    label: `Node ${i}`,
     color: `hsl(${Math.random() * 360}, 70%, 50%)`,
   }));
 
   const links = Array.from({ length: 15 }, () => ({
     source: `node-${Math.floor(Math.random() * 10)}`,
     target: `node-${Math.floor(Math.random() * 10)}`,
+    label: `Link ${Math.floor(Math.random() * 10)}`,
   }));
 
   return { nodes, links };
 };
 
 const NetworkVisualizationPage = () => {
-  const data = generateData();
+  const { nodes, links } = generateData();
 
   return (
-    (<div className="flex flex-col h-screen">
+    <div className="flex flex-col h-screen">
       <header className="bg-white p-4 shadow-md flex justify-between items-center">
         <div className="flex space-x-4">
           <Button variant="outline">
@@ -40,22 +42,39 @@ const NetworkVisualizationPage = () => {
         </Button>
       </header>
       <main className="flex-1 overflow-hidden p-4">
-        <div className="w-full h-full bg-white rounded shadow">
-          <div
-            nodes={data.nodes}
-            links={data.links}
-            margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-            repulsivity={50}
-            iterations={60}
-            nodeColor={node => node.color}
-            linkDistance={link => 30}
-            nodeSize={node => 5}
-            nodeBorderWidth={1}
-            nodeBorderColor={{ from: 'color', modifiers: [['darker', 0.8]] }}
-            linkThickness={link => 2}
-            motionStiffness={160}
-            motionDamping={12}
-          />
+        <div className="w-full h-full bg-white rounded shadow relative">
+          {nodes.map(node => (
+            <div
+              key={node.id}
+              className="absolute bg-blue-500 rounded-full text-white flex items-center justify-center"
+              style={{
+                width: '20px',
+                height: '20px',
+                transform: `translate(${Math.random() * 100}%, ${Math.random() * 100}%)`,
+              }}
+              title={node.label}
+            >
+              {node.label}
+            </div>
+          ))}
+          {links.map((link, index) => (
+            <svg key={index} className="absolute w-full h-full top-0 left-0">
+              <line
+                x1={`${Math.random() * 100}%`}
+                y1={`${Math.random() * 100}%`}
+                x2={`${Math.random() * 100}%`}
+                y2={`${Math.random() * 100}%`}
+                stroke="black"
+              />
+              <text
+                x={`${(Math.random() + Math.random()) / 2 * 100}%`}
+                y={`${(Math.random() + Math.random()) / 2 * 100}%`}
+                fill="black"
+              >
+                {link.label}
+              </text>
+            </svg>
+          ))}
         </div>
       </main>
       <footer className="bg-white p-4 shadow-md">
@@ -82,7 +101,7 @@ const NetworkVisualizationPage = () => {
           <p>Details about the node will be displayed here.</p>
         </DialogContent>
       </Dialog>
-    </div>)
+    </div>
   );
 };
 
