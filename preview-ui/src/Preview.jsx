ca9851519c76837b5f47979bcb64dbb47291b5fc
yz-yu <aryu0112@gmail.com>
2023-12-28 21:57:15 +0800

    [vx.dev] prompt: {   "prompt": "创建一个前端组件展示网站。\r\n左侧为菜单，有组件、hook、wiki 几大类，大类中可点击切换具体内容。\r\n右侧展示具体信息，若为组件，则展示简介、基本演示与示例代码、各属性对应的演示与示例代码、属性列表、slot 列表、事件列表；若为 hook，则展示简介、基本演示与示例代码、入参列表、返回值列表。若为 wiki 则展示文章内容。\n把 组件、hook、wiki 作为一级菜单，具体的组件比如 Accordion、Button 作为二级菜单。\r\n\r\n在目前的代码基础上，给 hook、wiki 也补充一下实际内容。",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -27,13 +27,36 @@ export default function ComponentShowcase() {
                 <AccordionItem value="card">
                   <AccordionTrigger>Card</AccordionTrigger>
                 </AccordionItem>
+                {/* More components can be added here */}
               </Accordion>
             </TabsContent>
             <TabsContent value="hooks" className="mt-4">
-              {/* Hooks list can be added here */}
+              <Accordion type="single" collapsible>
+                <AccordionItem value="useState">
+                  <AccordionTrigger>useState</AccordionTrigger>
+                </AccordionItem>
+                <AccordionItem value="useEffect">
+                  <AccordionTrigger>useEffect</AccordionTrigger>
+                </AccordionItem>
+                <AccordionItem value="useContext">
+                  <AccordionTrigger>useContext</AccordionTrigger>
+                </AccordionItem>
+                {/* More hooks can be added here */}
+              </Accordion>
             </TabsContent>
             <TabsContent value="wiki" className="mt-4">
-              {/* Wiki articles list can be added here */}
+              <Accordion type="single" collapsible>
+                <AccordionItem value="installation">
+                  <AccordionTrigger>Installation Guide</AccordionTrigger>
+                </AccordionItem>
+                <AccordionItem value="contribution">
+                  <AccordionTrigger>Contribution Guidelines</AccordionTrigger>
+                </AccordionItem>
+                <AccordionItem value="code-of-conduct">
+                  <AccordionTrigger>Code of Conduct</AccordionTrigger>
+                </AccordionItem>
+                {/* More wiki articles can be added here */}
+              </Accordion>
             </TabsContent>
           </Tabs>
         </ScrollArea>
@@ -75,10 +98,41 @@ export default function ComponentShowcase() {
             </Card>
           </TabsContent>
           <TabsContent value="hooks">
-            {/* Hook details can be added here */}
+            <Card>
+              <CardHeader>
+                <CardTitle>useState Hook</CardTitle>
+                <CardDescription>Allows you to add state to your functional components</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Basic Demo:</p>
+                {/* Basic demo of useState */}
+                <Separator />
+                <p>Example Code:</p>
+                <ScrollArea className="h-[200px] w-full rounded-md border p-4">
+                  <code>
+                    {`const [state, setState] = useState(initialState);`}
+                  </code>
+                </ScrollArea>
+                {/* More demos and code examples can be added here */}
+              </CardContent>
+            </Card>
           </TabsContent>
           <TabsContent value="wiki">
-            {/* Wiki content can be added here */}
+            <Card>
+              <CardHeader>
+                <CardTitle>Installation Guide</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <article>
+                  <h2>Step 1: Setting up the environment</h2>
+                  <p>Before you begin, make sure you have a fresh version of Node.js installed.</p>
+                  <h2>Step 2: Install the package</h2>
+                  <p>Run the following command to install the package:</p>
+                  <code>npm install your-package-name</code>
+                  {/* More steps and details can be added here */}
+                </article>
+              </CardContent>
+            </Card>
           </TabsContent>
         </Tabs>
       </div>
