bc5233b8d0592175d38882ff5a724f1e205edb32
yz-yu <aryu0112@gmail.com>
2023-12-28 23:54:54 +0800

    [vx.dev] prompt: {   "prompt": "Design a personal photography portfolio website.\r\nThe first part from top to bottom is the photographer’s nickname and introduction, using a main image to fill in the background. The second part is the photo display wall. The width of the photo is fixed, and the length is a random value after the original image is compressed proportionally. The block of each photo contains a set label. When you click on the image, a secondary page will pop up (including the original image and image introduction). , image tag, contact the author to request the original image button, like button, close secondary page button).\n",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,1 +1,81 @@
-export default function VxDev() { return <p>vx.dev placeholder</p>; }
+import { Dialog, DialogTrigger, DialogContent, DialogClose } from '@/components/ui/dialog';
+import { AspectRatio } from '@/components/ui/aspect-ratio';
+import { Button } from '@/components/ui/button';
+import { Mail, Heart, X } from 'lucide-react';
+import React from 'react';
+
+const photos = [
+  {
+    id: 1,
+    src: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6?w=800&dpr=2&q=80',
+    label: 'Nature',
+    description: 'A beautiful sunrise over the mountains.',
+  },
+  {
+    id: 2,
+    src: 'https://images.unsplash.com/photo-1571008887538-b36bb32f4571?w=800&dpr=2&q=80',
+    label: 'City Life',
+    description: 'The hustle and bustle of the city at noon.',
+  },
+  // Add more photos as needed
+];
+
+export default function PhotographyPortfolio() {
+  return (
+    <div className="flex flex-col">
+      {/* Photographer's Nickname and Introduction */}
+      <div className="relative h-screen w-full">
+        <img
+          src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?w=800&dpr=2&q=80"
+          alt="Main background"
+          className="absolute inset-0 object-cover w-full h-full"
+        />
+        <div className="absolute inset-0 bg-black bg-opacity-50" />
+        <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
+          <h1 className="text-6xl font-bold">John Doe</h1>
+          <p className="mt-4 text-xl">Capturing moments from today, creating memories for a lifetime.</p>
+        </div>
+      </div>
+
+      {/* Photo Display Wall */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+        {photos.map((photo) => (
+          <Dialog key={photo.id}>
+            <DialogTrigger asChild>
+              <div className="cursor-pointer">
+                <AspectRatio ratio={1 / (1 + Math.random())} className="bg-gray-200">
+                  <img src={photo.src} alt={`Photo by John Doe - ${photo.label}`} className="object-cover" />
+                </AspectRatio>
+                <div className="mt-2 text-center">
+                  <Button variant="outline">{photo.label}</Button>
+                </div>
+              </div>
+            </DialogTrigger>
+            <DialogContent>
+              <div className="flex flex-col items-center p-4">
+                <img src={photo.src} alt={`Photo by John Doe - ${photo.label}`} className="rounded-md" />
+                <p className="mt-4">{photo.description}</p>
+                <div className="mt-4 space-x-2">
+                  <Button variant="outline">
+                    <Mail className="w-5 h-5 mr-2" />
+                    Contact for Original
+                  </Button>
+                  <Button variant="outline">
+                    <Heart className="w-5 h-5 mr-2" />
+                    Like
+                  </Button>
+                </div>
+                <DialogClose asChild>
+                  <Button variant="outline" className="mt-4">
+                    <X className="w-5 h-5 mr-2" />
+                    Close
+                  </Button>
+                </DialogClose>
+              </div>
+            </DialogContent>
+          </Dialog>
+        ))}
+      </div>
+    </div>
+  );
+}
