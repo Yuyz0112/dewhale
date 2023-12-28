2afa2e60a16799d5471a199dd04af781e7189d18
yz-yu <aryu0112@gmail.com>
2023-12-28 22:42:04 +0800

    [vx.dev] prompt: {   "prompt": "中间是聊天界面，右侧可供选择的AI大模型：ChatGPT, Gemini，下边是输入部分，页面颜色为暗色\ntoggleGroupItem 没有对齐",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -7,25 +7,25 @@ import React from 'react';
 
 const ChatInterface = () => {
   return (
-    <div className="flex h-screen bg-gray-800 text-white">
+    <div className="flex h-screen bg-gray-900 text-white">
       {/* Chat area */}
       <div className="flex flex-col flex-1 p-4">
-        <ScrollArea className="flex-1 mb-4 p-4 bg-gray-700 rounded">
+        <ScrollArea className="flex-1 mb-4 p-4 bg-gray-800 rounded">
           {/* Messages will be here */}
         </ScrollArea>
         <div className="flex items-center space-x-2">
           <Input className="flex-1 rounded p-2 text-gray-800" placeholder="Type your message here..." />
-          <Button variant="solid" className="bg-blue-500 hover:bg-blue-600">
+          <Button variant="solid" className="bg-blue-600 hover:bg-blue-700">
             <ArrowRight className="w-5 h-5" />
           </Button>
         </div>
       </div>
 
       {/* AI Model Selection */}
-      <div className="w-64 bg-gray-700 p-4">
-        <ToggleGroup type="single" className="w-full">
-          <ToggleGroupItem value="chatgpt" className="mb-2 rounded p-2 text-center bg-gray-600 hover:bg-gray-500">ChatGPT</ToggleGroupItem>
-          <ToggleGroupItem value="gemini" className="rounded p-2 text-center bg-gray-600 hover:bg-gray-500">Gemini</ToggleGroupItem>
+      <div className="w-64 bg-gray-800 p-4">
+        <ToggleGroup type="single" className="w-full flex flex-col">
+          <ToggleGroupItem value="chatgpt" className="mb-2 rounded p-2 text-center bg-gray-700 hover:bg-gray-600">ChatGPT</ToggleGroupItem>
+          <ToggleGroupItem value="gemini" className="rounded p-2 text-center bg-gray-700 hover:bg-gray-600">Gemini</ToggleGroupItem>
         </ToggleGroup>
       </div>
     </div>
