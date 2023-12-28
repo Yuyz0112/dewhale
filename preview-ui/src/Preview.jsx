1b0dcb52468256434a82182599c9088130b87fe3
yz-yu <aryu0112@gmail.com>
2023-12-28 22:02:46 +0800

    [vx.dev] prompt: {   "prompt": "创建一个前端组件展示网站。\r\n左侧为菜单，有组件、hook、wiki 几大类，大类中可点击切换具体内容。\r\n右侧展示具体信息，若为组件，则展示简介、基本演示与示例代码、各属性对应的演示与示例代码、属性列表、slot 列表、事件列表；若为 hook，则展示简介、基本演示与示例代码、入参列表、返回值列表。若为 wiki 则展示文章内容。\n把 组件、hook、wiki 作为一级菜单，具体的组件比如 Accordion、Button 作为二级菜单。\r\n\r\n在目前的代码基础上，给 hook、wiki 也补充一下实际内容。\n不错，但是目前你使用的两个 tabs 互相独立，不能联动，左侧菜单不要使用 tabs",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,142 +1,93 @@
-import { ScrollArea } from '@/components/ui/scroll-area';
-import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
-import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from '@/components/ui/accordion';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { Separator } from '@/components/ui/separator';
+import { Accordion, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import React, { useState } from 'react';
 
 export default function ComponentShowcase() {
-  const [selectedTab, setSelectedTab] = useState('components');
+  const [selectedCategory, setSelectedCategory] = useState('components');
+  const [selectedItem, setSelectedItem] = useState('accordion');
+
+  const categories = {
+    components: ['Accordion', 'Button', 'Card'],
+    hooks: ['useState', 'useEffect', 'useContext'],
+    wiki: ['Installation Guide', 'Contribution Guidelines', 'Code of Conduct'],
+  };
+
+  const renderContent = () => {
+    switch (selectedCategory) {
+      case 'components':
+        return (
+          <Card>
+            <CardHeader>
+              <CardTitle>{selectedItem} Component</CardTitle>
+              <CardDescription>A collapsible component to show/hide content</CardDescription>
+            </CardHeader>
+            <CardContent>
+              {/* Component specific content goes here */}
+            </CardContent>
+          </Card>
+        );
+      case 'hooks':
+        return (
+          <Card>
+            <CardHeader>
+              <CardTitle>{selectedItem} Hook</CardTitle>
+              <CardDescription>Allows you to add state to your functional components</CardDescription>
+            </CardHeader>
+            <CardContent>
+              {/* Hook specific content goes here */}
+            </CardContent>
+          </Card>
+        );
+      case 'wiki':
+        return (
+          <Card>
+            <CardHeader>
+              <CardTitle>{selectedItem}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              {/* Wiki specific content goes here */}
+            </CardContent>
+          </Card>
+        );
+      default:
+        return null;
+    }
+  };
 
   return (
     <div className="flex h-screen">
       <div className="bg-white w-64 p-6 hidden sm:block">
         <ScrollArea className="h-full">
-          <Tabs defaultValue={selectedTab} onValueChange={setSelectedTab} className="flex flex-col">
-            <TabsList>
-              <TabsTrigger value="components">Components</TabsTrigger>
-              <TabsTrigger value="hooks">Hooks</TabsTrigger>
-              <TabsTrigger value="wiki">Wiki</TabsTrigger>
-            </TabsList>
-            <TabsContent value="components" className="mt-4">
-              <Accordion type="single" collapsible>
-                <AccordionItem value="accordion">
-                  <AccordionTrigger>Accordion</AccordionTrigger>
+          <div className="flex flex-col">
+            {Object.keys(categories).map((category) => (
+              <button
+                key={category}
+                className={`text-left ${selectedCategory === category ? 'font-bold' : ''}`}
+                onClick={() => {
+                  setSelectedCategory(category);
+                  setSelectedItem(categories[category][0]);
+                }}
+              >
+                {category.charAt(0).toUpperCase() + category.slice(1)}
+              </button>
+            ))}
+            <Separator />
+            <Accordion type="single" collapsible>
+              {categories[selectedCategory].map((item) => (
+                <AccordionItem key={item} value={item}>
+                  <AccordionTrigger onClick={() => setSelectedItem(item)}>
+                    {item}
+                  </AccordionTrigger>
                 </AccordionItem>
-                <AccordionItem value="button">
-                  <AccordionTrigger>Button</AccordionTrigger>
-                </AccordionItem>
-                <AccordionItem value="card">
-                  <AccordionTrigger>Card</AccordionTrigger>
-                </AccordionItem>
-                {/* More components can be added here */}
-              </Accordion>
-            </TabsContent>
-            <TabsContent value="hooks" className="mt-4">
-              <Accordion type="single" collapsible>
-                <AccordionItem value="useState">
-                  <AccordionTrigger>useState</AccordionTrigger>
-                </AccordionItem>
-                <AccordionItem value="useEffect">
-                  <AccordionTrigger>useEffect</AccordionTrigger>
-                </AccordionItem>
-                <AccordionItem value="useContext">
-                  <AccordionTrigger>useContext</AccordionTrigger>
-                </AccordionItem>
-                {/* More hooks can be added here */}
-              </Accordion>
-            </TabsContent>
-            <TabsContent value="wiki" className="mt-4">
-              <Accordion type="single" collapsible>
-                <AccordionItem value="installation">
-                  <AccordionTrigger>Installation Guide</AccordionTrigger>
-                </AccordionItem>
-                <AccordionItem value="contribution">
-                  <AccordionTrigger>Contribution Guidelines</AccordionTrigger>
-                </AccordionItem>
-                <AccordionItem value="code-of-conduct">
-                  <AccordionTrigger>Code of Conduct</AccordionTrigger>
-                </AccordionItem>
-                {/* More wiki articles can be added here */}
-              </Accordion>
-            </TabsContent>
-          </Tabs>
+              ))}
+            </Accordion>
+          </div>
         </ScrollArea>
       </div>
       <div className="flex-1 p-6 overflow-y-auto">
-        <Tabs defaultValue={selectedTab}>
-          <TabsContent value="components">
-            <Card>
-              <CardHeader>
-                <CardTitle>Accordion Component</CardTitle>
-                <CardDescription>A collapsible component to show/hide content</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Basic Demo:</p>
-                <Accordion type="single" collapsible>
-                  <AccordionItem value="item-1">
-                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                    <AccordionContent>
-                      Yes. It adheres to the WAI-ARIA design pattern.
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-                <Separator />
-                <p>Example Code:</p>
-                <ScrollArea className="h-[200px] w-full rounded-md border p-4">
-                  <code>
-                    {`<Accordion type="single" collapsible>
-  <AccordionItem value="item-1">
-    <AccordionTrigger>Is it accessible?</AccordionTrigger>
-    <AccordionContent>
-      Yes. It adheres to the WAI-ARIA design pattern.
-    </AccordionContent>
-  </AccordionItem>
-</Accordion>`}
-                  </code>
-                </ScrollArea>
-                {/* More demos and code examples can be added here */}
-              </CardContent>
-            </Card>
-          </TabsContent>
-          <TabsContent value="hooks">
-            <Card>
-              <CardHeader>
-                <CardTitle>useState Hook</CardTitle>
-                <CardDescription>Allows you to add state to your functional components</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Basic Demo:</p>
-                {/* Basic demo of useState */}
-                <Separator />
-                <p>Example Code:</p>
-                <ScrollArea className="h-[200px] w-full rounded-md border p-4">
-                  <code>
-                    {`const [state, setState] = useState(initialState);`}
-                  </code>
-                </ScrollArea>
-                {/* More demos and code examples can be added here */}
-              </CardContent>
-            </Card>
-          </TabsContent>
-          <TabsContent value="wiki">
-            <Card>
-              <CardHeader>
-                <CardTitle>Installation Guide</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <article>
-                  <h2>Step 1: Setting up the environment</h2>
-                  <p>Before you begin, make sure you have a fresh version of Node.js installed.</p>
-                  <h2>Step 2: Install the package</h2>
-                  <p>Run the following command to install the package:</p>
-                  <code>npm install your-package-name</code>
-                  {/* More steps and details can be added here */}
-                </article>
-              </CardContent>
-            </Card>
-          </TabsContent>
-        </Tabs>
+        {renderContent()}
       </div>
     </div>
   );
