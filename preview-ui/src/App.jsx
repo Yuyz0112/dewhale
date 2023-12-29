5b1eae8ff66ed0c889e8fcfc12bf32955ca9007e
linzhe141 <linzhe141@qq.com>
2023-12-29 15:49:31 +0800

    feat: add "code" syntax highlighting using shiki

--- preview-ui/src/App.jsx
+++ preview-ui/src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Button } from "@/components/ui/button";
 import ErrorBoundary from "./ErrorBoundary.jsx";
 import Preview from "./Preview.jsx";
-import PreviewStr from "./Preview.jsx?raw";
+import previewStr from "./Preview.jsx?raw";
+import { getHighlighter, setCDN } from "shiki";
 
 export default function Home() {
   const [showCanvas, setShowCanvas] = useState(true);
   const [copied, setCopied] = useState(false);
+  const [code, setCode] = useState("");
 
+  function setCodeHighlighter() {
+    setCDN("/shiki/");
+    getHighlighter({ theme: "one-dark-pro", langs: ["jsx"] })
+      .then((h) => {
+        const html = h.codeToHtml(previewStr, { lang: "jsx" });
+        setCode(html);
+      })
+      .catch((error) => {
+        setCode(error);
+      });
+  }
+
+  useEffect(() => {
+    setCodeHighlighter();
+  }, []);
   return (
     <main className="vx-dev-wrapper relative">
       <div className="flex justify-end p-1">
@@ -25,7 +42,7 @@ export default function Home() {
       ) : (
         <pre className="bg-gray-100 mx-1 p-1 rounded overflow-auto relative text-xs">
           <CopyToClipboard
-            text={PreviewStr}
+            text={previewStr}
             onCopy={() => {
               setCopied(true);
               setTimeout(() => setCopied(false), 2000);
@@ -35,7 +52,7 @@ export default function Home() {
               {copied ? "Copied!" : "Copy Code"}
             </Button>
           </CopyToClipboard>
-          <code>{PreviewStr}</code>
+          <div dangerouslySetInnerHTML={{ __html: code }}></div>
         </pre>
       )}
     </main>
