9b53ecde96790dcb1759b25ef24a04ea31a23e16
yz-yu <aryu0112@gmail.com>
2024-03-17 15:31:47 +0800

    [Dewhale] prompt: {   "prompt": "design a UI to display the follow data\r\n\r\n```\r\n{\r\n  \"equipments\": [\r\n    \"自身重量\",\r\n    \"单杠\"\r\n  ],\r\n  \"muscles\": [\r\n    \"肩部\",\r\n    \"胸肌\",\r\n    \"腹肌\"\r\n  ],\r\n  \"exercises\": [\r\n    {\r\n      \"title\": \"Dips\", \"url\": \"https://musclewiki.com/barbell/male/biceps/barbell-curl\"\r\n    },\r\n    {\r\n      \"title\": \"Push Up\", \"url\": \"https://musclewiki.com/barbell/male/biceps/push-up\"\r\n    },\r\n    {\r\n      \"title\": \"Burpee\", \"url\": \"https://musclewiki.com/barbell/male/biceps/burpee\"\r\n    },\r\n  ]\r\n}\r\n```\n",   "images": [] }

--- preview-ui/src/Preview.jsx
+++ preview-ui/src/Preview.jsx
@@ -1,1 +1,57 @@
-export default function Preview() { return <p>Dewhale placeholder</p>; }
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { List, Home, Link } from 'lucide-react';
+import React from 'react';
+
+export default function ExerciseUI() {
+  return (
+    (<div className="flex flex-col gap-6 p-6">
+      <Card>
+        <CardHeader>
+          <CardTitle>Equipments</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <List>
+            <div>自身重量</div>
+            <div>单杠</div>
+          </List>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Muscles</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <List>
+            <div>肩部</div>
+            <div>胸肌</div>
+            <div>腹肌</div>
+          </List>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Exercises</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <List>
+            <div>
+              <Link href="https://musclewiki.com/barbell/male/biceps/barbell-curl" isExternal>
+                Dips
+              </Link>
+            </div>
+            <div>
+              <Link href="https://musclewiki.com/barbell/male/biceps/push-up" isExternal>
+                Push Up
+              </Link>
+            </div>
+            <div>
+              <Link href="https://musclewiki.com/barbell/male/biceps/burpee" isExternal>
+                Burpee
+              </Link>
+            </div>
+          </List>
+        </CardContent>
+      </Card>
+    </div>)
+  );
+}
