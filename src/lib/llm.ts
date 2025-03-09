824911e128d47ad21464fe695e688b7a58c0d841
Yanzhen Yu <yanzhen@smartx.com>
2025-03-09 14:28:04 +0800

    testing

---
+++ src/lib/llm.ts
@@ -0,0 +1,21 @@
+import "jsr:@std/dotenv@0.225.3/load";
+import { google } from "npm:@ai-sdk/google@1.1.20";
+import {
+  generateText,
+  streamText,
+  tool,
+  jsonSchema,
+  ToolSet,
+} from "npm:ai@4.1.54";
+
+export function getModel(provider: string, model: string) {
+  switch (provider) {
+    case "google":
+      return google(model);
+    default:
+      throw new Error(`Invalid provider "${provider}"`);
+  }
+}
+
+export { generateText, streamText, tool, jsonSchema };
+export type { ToolSet };
